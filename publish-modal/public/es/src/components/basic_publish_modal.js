997523e6841d916860ed6ad4e0571972d51f615f
hugh-obrien <hugh.obrien@overleaf.com>
2018-04-25 10:17:17 +0100

    remove affix menu for now

--- publish-modal/public/es/src/components/basic_publish_modal.js
+++ publish-modal/public/es/src/components/basic_publish_modal.js
@@ -1,13 +1,8 @@
 import React, { PropTypes, Component } from 'react'
-// import $ from 'jquery'
 import PublishAffix from './publish_affix'
 import PublishSections from './publish_sections'
 
 export default class BasicPublishModal extends Component {
-  componentDidMount () {
-    // $('#publish-modal').trigger('activate-affix-modal')
-  }
-
   render () {
     const { entries } = this.props
     return (
@@ -15,8 +10,7 @@ export default class BasicPublishModal extends Component {
         <div
           id='publish-table'
           className='modal-body-content row content-as-table'>
-          <PublishAffix categories={entries} />
-          <div className='col-md-9 col-affixed'>
+          <div className='col-md-12'>
             <PublishSections entries={entries}
               onSwitch={this.props.onSwitch} />
           </div>
