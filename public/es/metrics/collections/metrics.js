bdfc137f423687b1c22583a42e7141ff908139fc
Alberto Fernández Capel <alberto.fernandez-capel@overleaf.com>
2018-05-14 14:06:09 +0100

    Do not include resource_id in the root /graphs request
    Analytics rejects the params in that endpoint, it only works
    for individual graphs, not to get the list.
    
    In v1 the proxy removed the param, but it's easier if we don't include
    it to begin with.

--- public/es/metrics/collections/metrics.js
+++ public/es/metrics/collections/metrics.js
@@ -6,9 +6,6 @@ export const Metrics = Backbone.Collection.extend({
   initialize: function() {
     this.url = METRICS_ENDPOINT + '/';
     var queryString = '';
-    if(METRICS_RESOURCE_ID && METRICS_RESOURCE_ID.length > 0) {
-      queryString += '?resource_id=' + METRICS_RESOURCE_ID;
-    }
     if(METRICS_RESOURCE_TYPE && METRICS_RESOURCE_TYPE.length > 0) {
       queryString += (queryString.length === 0) ? '?' : '&';
       queryString += 'resource_type=' + METRICS_RESOURCE_TYPE;
