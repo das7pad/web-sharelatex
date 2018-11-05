acd219b58671bdbcbd6e1c3a63ae2cea3daca10b
decaffeinate <ali@alasdairsmith.co.uk>
2018-11-05 09:41:44 +0000

    decaffeinate: Convert blog.coffee and 1 other file to JS

--- modules/cms/public/coffee/main/blog.js
+++ modules/cms/public/coffee/main/blog.js
@@ -1,4 +1,8 @@
-define [
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+define([
 	'directives/mathjax'
-], (App) ->
-	App.controller "BlogController", () ->
+], App => App.controller("BlogController", function() {}));
--- modules/cms/public/coffee/main/index.js
+++ modules/cms/public/coffee/main/index.js
@@ -1,4 +1,8 @@
-define [
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+define([
 	'base'
-], (App) ->
-	App.controller "CmsController", () ->
+], App => App.controller("CmsController", function() {}));
