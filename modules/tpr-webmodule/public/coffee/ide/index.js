61e6319662f6d47f0e2139d7e353c1b7649d9378
decaffeinate <ali@alasdairsmith.co.uk>
2018-11-05 09:42:41 +0000

    decaffeinate: Run post-processing cleanups on MendeleyLinkedFileModalController.coffee and 4 other files

--- modules/tpr-webmodule/public/coffee/ide/index.js
+++ modules/tpr-webmodule/public/coffee/ide/index.js
@@ -1,3 +1,5 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
 define([
 	"ide/tpr-webmodule/controllers/ReferencesController",
 	"ide/tpr-webmodule/controllers/MendeleyLinkedFileModalController"
