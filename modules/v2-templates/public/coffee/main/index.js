34b7df16ba9a47aa9d6faabbd709418e5f28190c
decaffeinate <ali@alasdairsmith.co.uk>
2018-11-05 09:42:46 +0000

    decaffeinate: Run post-processing cleanups on addsearch.coffee and 2 other files

--- modules/v2-templates/public/coffee/main/index.js
+++ modules/v2-templates/public/coffee/main/index.js
@@ -1,3 +1,5 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
 define([
   "main/v2-templates/template_page_controller",
   "main/v2-templates/directives/addsearch"
