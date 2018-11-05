4b8cd79cabedc2f647f12f29ecae1328fd2a12af
decaffeinate <ali@alasdairsmith.co.uk>
2018-11-05 09:42:36 +0000

    decaffeinate: Run post-processing cleanups on TemplatesController.coffee and 2 other files

--- modules/templates/public/coffee/ide/index.js
+++ modules/templates/public/coffee/ide/index.js
@@ -1,3 +1,5 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
 define([
 	"ide/templates/controllers/TemplatesController"
 ], function() {});
