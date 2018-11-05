dd838f6c67b6b3c9b2147f9127276a0332ee78ef
decaffeinate <ali@alasdairsmith.co.uk>
2018-11-05 09:42:05 +0000

    decaffeinate: Convert index.coffee to JS

--- modules/overleaf-account-merge/public/coffee/main/index.js
+++ modules/overleaf-account-merge/public/coffee/main/index.js
@@ -1,24 +1,38 @@
-define [
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * DS103: Rewrite code to no longer use __guard__
+ * DS207: Consider shorter variations of null checks
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+define([
 	"base"
-], (App) ->
+], App =>
 
-	App.controller "SharelatexAccountMergeCheckerController", ($scope, $http) ->
-		$scope.hasOlAccount = null
-		$scope.olEmail = ""
-		$scope.errorCode = null
-		$scope.success = null
+	App.controller("SharelatexAccountMergeCheckerController", function($scope, $http) {
+		$scope.hasOlAccount = null;
+		$scope.olEmail = "";
+		$scope.errorCode = null;
+		$scope.success = null;
 
-		$scope.submitEmail = () ->
-			return if !$scope.olEmail
-			data = {
-				overleafEmail: $scope.olEmail
+		return $scope.submitEmail = function() {
+			if (!$scope.olEmail) { return; }
+			const data = {
+				overleafEmail: $scope.olEmail,
 				_csrf: window.csrfToken
-			}
-			$scope.errorCode = null
-			$http.post("/account-merge/email/overleaf", data)
-				.then (resp) ->
-					$scope.errorCode = null
-					$scope.success = true
-				.catch (resp) ->
-					$scope.errorCode = resp?.data?.errorCode || 'default_error'
-					$scope.success = false
+			};
+			$scope.errorCode = null;
+			return $http.post("/account-merge/email/overleaf", data)
+				.then(function(resp) {
+					$scope.errorCode = null;
+					return $scope.success = true;}).catch(function(resp) {
+					$scope.errorCode = __guard__(resp != null ? resp.data : undefined, x => x.errorCode) || 'default_error';
+					return $scope.success = false;
+			});
+		};
+	})
+);
+
+function __guard__(value, transform) {
+  return (typeof value !== 'undefined' && value !== null) ? transform(value) : undefined;
+}
