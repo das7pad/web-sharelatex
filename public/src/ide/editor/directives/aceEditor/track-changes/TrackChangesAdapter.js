3c6efc263a6fa48534eb634573ef79746fb899e3
Chrystal Griffiths <chrystal.griffiths@overleaf.com>
2018-11-28 17:47:02 +0000

    Decaffeination and cleanup

--- public/src/ide/editor/directives/aceEditor/track-changes/TrackChangesAdapter.js
+++ public/src/ide/editor/directives/aceEditor/track-changes/TrackChangesAdapter.js
@@ -7,207 +7,197 @@
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
 define(['ace/ace', 'ide/editor/AceShareJsCodec'], function(
-  Ace,
+  _ignore,
   AceShareJsCodec
 ) {
-  let TrackChangesAdapter
-  return (TrackChangesAdapter = (function() {
-    let Range
-    TrackChangesAdapter = class TrackChangesAdapter {
-      static initClass() {
-        ;({ Range } = ace.require('ace/range'))
-      }
+  const { Range } = ace.require('ace/range')
+  class TrackChangesAdapter {
+    constructor(editor) {
+      this.editor = editor
+      this.changeIdToMarkerIdMap = {}
+    }
 
-      constructor(editor) {
-        this.editor = editor
-        this.changeIdToMarkerIdMap = {}
-      }
-
-      clearAnnotations() {
-        const session = this.editor.getSession()
-        for (let change_id in this.changeIdToMarkerIdMap) {
-          const markers = this.changeIdToMarkerIdMap[change_id]
-          for (let marker_name in markers) {
-            const marker_id = markers[marker_name]
-            session.removeMarker(marker_id)
-          }
-        }
-        this.changeIdToMarkerIdMap = {}
-      }
-
-      onInsertAdded(change) {
-        const start = this.shareJsOffsetToAcePosition(change.op.p)
-        const end = this.shareJsOffsetToAcePosition(
-          change.op.p + change.op.i.length
-        )
-
-        const session = this.editor.getSession()
-        const background_range = new Range(
-          start.row,
-          start.column,
-          end.row,
-          end.column
-        )
-        const background_marker_id = session.addMarker(
-          background_range,
-          'track-changes-marker track-changes-added-marker',
-          'text'
-        )
-        const callout_marker_id = this.createCalloutMarker(
-          start,
-          'track-changes-added-marker-callout'
-        )
-
-        this.changeIdToMarkerIdMap[change.id] = {
-          background_marker_id,
-          callout_marker_id
+    clearAnnotations() {
+      const session = this.editor.getSession()
+      for (let change_id in this.changeIdToMarkerIdMap) {
+        const markers = this.changeIdToMarkerIdMap[change_id]
+        for (let marker_name in markers) {
+          const marker_id = markers[marker_name]
+          session.removeMarker(marker_id)
         }
       }
+      this.changeIdToMarkerIdMap = {}
+    }
 
-      onDeleteAdded(change) {
-        const position = this.shareJsOffsetToAcePosition(change.op.p)
-        const session = this.editor.getSession()
+    onInsertAdded(change) {
+      const start = this.shareJsOffsetToAcePosition(change.op.p)
+      const end = this.shareJsOffsetToAcePosition(
+        change.op.p + change.op.i.length
+      )
 
-        const markerLayer = this.editor.renderer.$markerBack
-        const klass = 'track-changes-marker track-changes-deleted-marker'
-        const background_range = this.makeZeroWidthRange(position)
-        const background_marker_id = session.addMarker(
-          background_range,
-          klass,
-          (html, range, left, top, config) =>
-            markerLayer.drawSingleLineMarker(
-              html,
-              range,
-              `${klass} ace_start`,
-              config,
-              0,
-              ''
-            )
-        )
+      const session = this.editor.getSession()
+      const background_range = new Range(
+        start.row,
+        start.column,
+        end.row,
+        end.column
+      )
+      const background_marker_id = session.addMarker(
+        background_range,
+        'track-changes-marker track-changes-added-marker',
+        'text'
+      )
+      const callout_marker_id = this.createCalloutMarker(
+        start,
+        'track-changes-added-marker-callout'
+      )
 
-        const callout_marker_id = this.createCalloutMarker(
-          position,
-          'track-changes-deleted-marker-callout'
-        )
-
-        return (this.changeIdToMarkerIdMap[change.id] = {
-          background_marker_id,
-          callout_marker_id
-        })
-      }
-
-      onInsertRemoved(change) {
-        const {
-          background_marker_id,
-          callout_marker_id
-        } = this.changeIdToMarkerIdMap[change.id]
-        delete this.changeIdToMarkerIdMap[change.id]
-        const session = this.editor.getSession()
-        session.removeMarker(background_marker_id)
-        session.removeMarker(callout_marker_id)
-      }
-
-      onDeleteRemoved(change) {
-        const {
-          background_marker_id,
-          callout_marker_id
-        } = this.adapter.changeIdToMarkerIdMap[change.id]
-        delete this.adapter.changeIdToMarkerIdMap[change.id]
-
-        const session = this.editor.getSession()
-        session.removeMarker(background_marker_id)
-        return session.removeMarker(callout_marker_id)
-      }
-
-      onChangeMoved(change) {
-        let end
-        const start = this.shareJsOffsetToAcePosition(change.op.p)
-        if (change.op.i != null) {
-          end = this.shareJsOffsetToAcePosition(
-            change.op.p + change.op.i.length
-          )
-        } else {
-          end = start
-        }
-        return this.updateMarker(change.id, start, end)
-      }
-
-      updateMarker(change_id, start, end) {
-        if (this.changeIdToMarkerIdMap[change_id] == null) {
-          return
-        }
-        const session = this.editor.getSession()
-        const markers = session.getMarkers()
-        const {
-          background_marker_id,
-          callout_marker_id
-        } = this.changeIdToMarkerIdMap[change_id]
-        if (
-          background_marker_id != null &&
-          markers[background_marker_id] != null
-        ) {
-          const background_marker = markers[background_marker_id]
-          background_marker.range.start = start
-          background_marker.range.end = end
-        }
-        if (callout_marker_id != null && markers[callout_marker_id] != null) {
-          const callout_marker = markers[callout_marker_id]
-          callout_marker.range.start = start
-          return (callout_marker.range.end = start)
-        }
-      }
-
-      shareJsOffsetToAcePosition(offset) {
-        const lines = this.editor
-          .getSession()
-          .getDocument()
-          .getAllLines()
-        return AceShareJsCodec.shareJsOffsetToAcePosition(offset, lines)
-      }
-
-      createCalloutMarker(position, klass) {
-        let callout_marker_id
-        const session = this.editor.getSession()
-        const callout_range = this.makeZeroWidthRange(position)
-        const markerLayer = this.editor.renderer.$markerBack
-        return (callout_marker_id = session.addMarker(
-          callout_range,
-          klass,
-          (html, range, left, top, config) =>
-            markerLayer.drawSingleLineMarker(
-              html,
-              range,
-              `track-changes-marker-callout ${klass} ace_start`,
-              config,
-              0,
-              'width: auto; right: 0;'
-            )
-        ))
-      }
-
-      makeZeroWidthRange(position) {
-        const ace_range = new Range(
-          position.row,
-          position.column,
-          position.row,
-          position.column
-        )
-        // Our delete marker is zero characters wide, but Ace doesn't draw ranges
-        // that are empty. So we monkey patch the range to tell Ace it's not empty.
-        // We do want to claim to be empty if we're off screen after clipping rows though.
-        // This is the code we need to trick:
-        //   var range = marker.range.clipRows(config.firstRow, config.lastRow);
-        //   if (range.isEmpty()) continue;
-        ace_range.clipRows = function(first_row, last_row) {
-          this.isEmpty = function() {
-            return first_row > this.end.row || last_row < this.start.row
-          }
-          return this
-        }
-        return ace_range
+      this.changeIdToMarkerIdMap[change.id] = {
+        background_marker_id,
+        callout_marker_id
       }
     }
-    TrackChangesAdapter.initClass()
-    return TrackChangesAdapter
-  })())
+
+    onDeleteAdded(change) {
+      const position = this.shareJsOffsetToAcePosition(change.op.p)
+      const session = this.editor.getSession()
+
+      const markerLayer = this.editor.renderer.$markerBack
+      const klass = 'track-changes-marker track-changes-deleted-marker'
+      const background_range = this.makeZeroWidthRange(position)
+      const background_marker_id = session.addMarker(
+        background_range,
+        klass,
+        (html, range, left, top, config) =>
+          markerLayer.drawSingleLineMarker(
+            html,
+            range,
+            `${klass} ace_start`,
+            config,
+            0,
+            ''
+          )
+      )
+
+      const callout_marker_id = this.createCalloutMarker(
+        position,
+        'track-changes-deleted-marker-callout'
+      )
+
+      return (this.changeIdToMarkerIdMap[change.id] = {
+        background_marker_id,
+        callout_marker_id
+      })
+    }
+
+    onInsertRemoved(change) {
+      const {
+        background_marker_id,
+        callout_marker_id
+      } = this.changeIdToMarkerIdMap[change.id]
+      delete this.changeIdToMarkerIdMap[change.id]
+      const session = this.editor.getSession()
+      session.removeMarker(background_marker_id)
+      session.removeMarker(callout_marker_id)
+    }
+
+    onDeleteRemoved(change) {
+      const {
+        background_marker_id,
+        callout_marker_id
+      } = this.adapter.changeIdToMarkerIdMap[change.id]
+      delete this.adapter.changeIdToMarkerIdMap[change.id]
+
+      const session = this.editor.getSession()
+      session.removeMarker(background_marker_id)
+      return session.removeMarker(callout_marker_id)
+    }
+
+    onChangeMoved(change) {
+      let end
+      const start = this.shareJsOffsetToAcePosition(change.op.p)
+      if (change.op.i != null) {
+        end = this.shareJsOffsetToAcePosition(change.op.p + change.op.i.length)
+      } else {
+        end = start
+      }
+      return this.updateMarker(change.id, start, end)
+    }
+
+    updateMarker(change_id, start, end) {
+      if (this.changeIdToMarkerIdMap[change_id] == null) {
+        return
+      }
+      const session = this.editor.getSession()
+      const markers = session.getMarkers()
+      const {
+        background_marker_id,
+        callout_marker_id
+      } = this.changeIdToMarkerIdMap[change_id]
+      if (
+        background_marker_id != null &&
+        markers[background_marker_id] != null
+      ) {
+        const background_marker = markers[background_marker_id]
+        background_marker.range.start = start
+        background_marker.range.end = end
+      }
+      if (callout_marker_id != null && markers[callout_marker_id] != null) {
+        const callout_marker = markers[callout_marker_id]
+        callout_marker.range.start = start
+        return (callout_marker.range.end = start)
+      }
+    }
+
+    shareJsOffsetToAcePosition(offset) {
+      const lines = this.editor
+        .getSession()
+        .getDocument()
+        .getAllLines()
+      return AceShareJsCodec.shareJsOffsetToAcePosition(offset, lines)
+    }
+
+    createCalloutMarker(position, klass) {
+      let callout_marker_id
+      const session = this.editor.getSession()
+      const callout_range = this.makeZeroWidthRange(position)
+      const markerLayer = this.editor.renderer.$markerBack
+      return (callout_marker_id = session.addMarker(
+        callout_range,
+        klass,
+        (html, range, left, top, config) =>
+          markerLayer.drawSingleLineMarker(
+            html,
+            range,
+            `track-changes-marker-callout ${klass} ace_start`,
+            config,
+            0,
+            'width: auto; right: 0;'
+          )
+      ))
+    }
+
+    makeZeroWidthRange(position) {
+      const ace_range = new Range(
+        position.row,
+        position.column,
+        position.row,
+        position.column
+      )
+      // Our delete marker is zero characters wide, but Ace doesn't draw ranges
+      // that are empty. So we monkey patch the range to tell Ace it's not empty.
+      // We do want to claim to be empty if we're off screen after clipping rows though.
+      // This is the code we need to trick:
+      //   var range = marker.range.clipRows(config.firstRow, config.lastRow);
+      //   if (range.isEmpty()) continue;
+      ace_range.clipRows = function(first_row, last_row) {
+        this.isEmpty = function() {
+          return first_row > this.end.row || last_row < this.start.row
+        }
+        return this
+      }
+      return ace_range
+    }
+  }
+  return TrackChangesAdapter
 })
