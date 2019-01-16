das7pad/web-sharelatex

commit 819cfa7d7023c9fa50c2095b8f655c19a321c237

Include anchor in URL and section name in UI for wiki search result

modules/support/public/src/main/index.js
@@ -66,9 +66,15 @@ define([
 					const page_underscored = __guard__(hit != null ? hit.pageName : undefined, x => x.replace(/\s/g,'_'));
 					const page_slug = encodeURIComponent(page_underscored);
 					const page_path = hit.kb ? 'how-to' : 'latex'
+					const section_exists = hit.sectionName && hit.sectionName != ''
+					const page_anchor = section_exists ? `#${hit.sectionName.replace(/\s/g, '_')}` : ``
+					let page_name = hit._highlightResult.pageName.value
+					if (section_exists) {
+						page_name += ' ― ' + hit.sectionName
+					}
 					result.push(suggestion = {
-						url : `/learn/${page_path}/${page_slug}`,
-						name : hit._highlightResult.pageName.value
+						url : `/learn/${page_path}/${page_slug}${page_anchor}`,
+						name : page_name
 					});
 				}
 				return result;

commit a9dcce7a952f3eecc11320d43e79945032a733d1

Bring back commit 819cfa7 accidentally overwritten by linting

modules/support/public/src/main/index.js
@@ -80,9 +80,18 @@ define(['base', 'libs/platform', 'services/algolia-search'], function(
             x => x.replace(/\s/g, '_')
           )
           const pageSlug = encodeURIComponent(pageUnderscored)
+          const pagePath = hit.kb ? 'how-to' : 'latex'
+          const sectionExists = hit.sectionExists && hit.sectionName !== ''
+          const pageAnchor = sectionExists
+            ? `#${hit.sectionName.replace(/\s/g, '_')}`
+            : ''
+          let pageName = hit._highlightResult.pageName.value
+          if (sectionExists) {
+            pageName += ' - ' + hit.sectionName
+          }
           result.push({
-            url: `/learn/how-to/${pageSlug}`,
-            name: hit._highlightResult.pageName.value
+            url: `/learn/${pagePath}/${pageSlug}${pageAnchor}`,
+            name: pageName
           })
         }
         return result