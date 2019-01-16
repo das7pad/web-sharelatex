7fae3703d695e63e553317ea7e59a4431b484329
Alasdair Smith <ali@alasdairsmith.co.uk>
2019-01-16 10:42:15 +0000

    Fix commit 7da9ef4 accidentally overwritten by linting

--- modules/support/public/src/main/index.js
+++ modules/support/public/src/main/index.js
@@ -158,7 +158,7 @@ define(['base', 'libs/platform', 'services/algolia-search'], function(
     var _setupSuggestionsWatcher = () =>
       $scope.$watch('form.subject', function(newVal, oldVal) {
         if (newVal && newVal !== oldVal && newVal.length > 3) {
-          return algoliaSearch.searchKB(newVal, _handleSearchResults, {
+          return algoliaSearch.searchWiki(newVal, _handleSearchResults, {
             hitsPerPage: 3,
             typoTolerance: 'strict'
           })

das7pad/web-sharelatex

commit 7da9ef4252e99618abbfc33d43b14799dd63e80f

Update contact form to use main index
KB index is being deprecated and all wiki pages will be indexed together.

modules/support/public/src/main/index.js
@@ -119,7 +119,7 @@ define([
 		var _setupSuggestionsWatcher = () =>
 			$scope.$watch("form.subject", function(newVal, oldVal) {
 				if (newVal && (newVal !== oldVal) && (newVal.length > 3)) {
-					return algoliaSearch.searchKB(newVal, _handleSearchResults, {
+					return algoliaSearch.searchWiki(newVal, _handleSearchResults, {
 						hitsPerPage: 3,
 						typoTolerance: 'strict'
 					});

public/src/services/algolia-search.js
@@ -50,6 +50,7 @@ define(['base'], App =>
       )
     }
 
+    // searchKB is deprecated
     const service = {
       searchWiki: wikiIdx ? wikiIdx.search.bind(wikiIdx) : null,
       searchKB: kbIdx ? kbIdx.search.bind(kbIdx) : null

commit 7fae3703d695e63e553317ea7e59a4431b484329

Fix commit 7da9ef4 accidentally overwritten by linting

modules/support/public/src/main/index.js
@@ -158,7 +158,7 @@ define(['base', 'libs/platform', 'services/algolia-search'], function(
     var _setupSuggestionsWatcher = () =>
       $scope.$watch('form.subject', function(newVal, oldVal) {
         if (newVal && newVal !== oldVal && newVal.length > 3) {
-          return algoliaSearch.searchKB(newVal, _handleSearchResults, {
+          return algoliaSearch.searchWiki(newVal, _handleSearchResults, {
             hitsPerPage: 3,
             typoTolerance: 'strict'
           })