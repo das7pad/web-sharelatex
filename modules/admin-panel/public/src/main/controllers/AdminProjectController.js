be27a683f2d06f93d4de3c9561d3bb56d75423c3
Shane Kilkelly <shane@kilkelly.me>
2019-01-18 11:12:37 +0000

    Smarter json-data injection into admin/project page

--- modules/admin-panel/public/src/main/controllers/AdminProjectController.js
+++ modules/admin-panel/public/src/main/controllers/AdminProjectController.js
@@ -1,11 +1,7 @@
 define(['base', 'libs/md5'], function(App) {
-  App.controller('AdminProjectController', function(
-    $scope,
-    $timeout,
-    $modal,
-    queuedHttp
-  ) {
-    $scope.project = window.data.project
+  App.controller('AdminProjectController', function($scope) {
+    let { project } = JSON.parse($('#admin-project-data').text())
+    $scope.project = project
     $scope.editingBrandVariationId = false
   })
 })
