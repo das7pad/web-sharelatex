4000f3f1e3b655bcf8fc8382ab6810af3e7ffd8c
Ersun Warncke <ersun.warncke@overleaf.com>
2019-01-18 13:07:59 -0400

    add recaptcha validate middleware to reg and add recaptcha v3

---
+++ public/src/services/validateCaptchaV3.js
@@ -0,0 +1,24 @@
+define(['base'], function(App) {
+  return App.factory('validateCaptchaV3', function() {
+    var validateCaptchaV3 = function validateCaptchaV3(actionName, callback) {
+      if (callback == null) {
+        callback = function callback(response) {}
+      }
+      if (typeof grecaptcha === 'undefined' || grecaptcha === null) {
+        return
+      }
+      if (
+        typeof ExposedSettings === 'undefined' ||
+        typeof ExposedSettings.recaptchaSiteKeyV3 === 'undefined'
+      ) {
+        return
+      }
+      grecaptcha.ready(function() {
+        grecaptcha
+          .execute(ExposedSettings.recaptchaSiteKeyV3, { action: actionName })
+          .then(callback)
+      })
+    }
+    return validateCaptchaV3
+  })
+})
