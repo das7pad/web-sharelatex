b11df376f1428ad59ecb63f0331da9c402313650
Douglas Lovell <doug@wbreeze.com>
2019-01-28 13:05:55 -0300

    Refactor journals list request into a promise

--- modules/publish-modal/public/es/index.js
+++ modules/publish-modal/public/es/index.js
@@ -51,18 +51,55 @@ function _getJournalsAndRender(
   brandVariationId,
   initialShown
 ) {
-  $.ajax({
-    url: url,
-    type: 'GET',
-    success: function(jsonResponse) {
-      var entries = JSON.parse(jsonResponse)
-      var props = {
-        entries: entries,
-        initialShown: initialShown,
-        initParams: initParams,
-        brandVariationId: brandVariationId
-      }
-      ReactDOM.render(React.createElement(PublishModal, props), rootEl)
+  const showPublishModal = jsonResponse => {
+    let entries = JSON.parse(jsonResponse)
+    let props = {
+      entries: entries,
+      initialShown: initialShown,
+      initParams: initParams,
+      brandVariationId: brandVariationId
     }
+    ReactDOM.render(React.createElement(PublishModal, props), rootEl)
+  }
+  const showError = jsonResponse => {
+    // TODO create an error component to render here instead
+    let props = {
+      entries: entries,
+      initialShown: initialShown,
+      initParams: initParams,
+      brandVariationId: brandVariationId
+    }
+    ReactDOM.render(React.createElement(PublishModal, props), rootEl)
+  }
+  promiseJournalsRequest(url).then(showPublishModal, showError)
+}
+
+function promiseJournalsRequest(url) {
+  return new Promise((resolve, reject) => {
+    console.log('RETRIEVE JOURNAL LIST ', url)
+    $.ajax({
+      url: url,
+      type: 'GET'
+    })
+      .done(resolve)
+      .fail(reject)
+  })
+}
+
+function promisePriorSubmissionRequest(projectId) {
+  return new Promise((resolve, reject) => {
+    let url = `/latest_template/{projectId}`
+    console.log('RETRIEVE LATEST SUBMISSION ', url)
+    $.ajax({
+      url: url,
+      type: 'GET',
+      success: function(jsonResponse) {
+        var template = JSON.parse(jsonResponse)
+        console.log('LATEST TEMPLATE RETURNED ', template)
+        props.description = template.description
+        props.author = template.author
+        props.license = template.license
+      }
+    })
   })
 }
