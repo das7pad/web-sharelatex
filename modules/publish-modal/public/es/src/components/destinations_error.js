fa620b3c2c420c599a17826788741a71c2f44401
Douglas Lovell <doug@wbreeze.com>
2019-01-31 14:46:10 -0300

    Update wording of non-retrieval error

--- modules/publish-modal/public/es/src/components/destinations_error.js
+++ modules/publish-modal/public/es/src/components/destinations_error.js
@@ -5,14 +5,13 @@ export default class DestinationsError extends Component {
     return (
       <span>
         <p>
-          We had trouble retrieving our list of submission destinations. If you
-          give us a minute, retry, and still find this error, please notify us
-          using our
-          <a href="/contact">contact form</a>
-          or via email to
-          <a href="mailto:support@overleaf.com">support@overleaf.com</a>
+          Sorry, we had trouble retrieving our list of submission destinations.
+          If you give us a minute, retry, and still find this error, please
+          notify support at{' '}
+          <a href="mailto:support@overleaf.com?subject=Unable to submit">
+            support@overleaf.com
+          </a>
         </p>
-        <p>Please excuse. We'll try to make this go away quickly.</p>
       </span>
     )
   }
