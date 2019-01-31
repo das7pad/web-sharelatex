cc79d0dfda90fa635a1e409d020ce98986499e5a
Henry Oswald <henry.oswald@sharelatex.com>
2019-01-31 13:56:27 +0000

    simplify install deps

--- install_deps.sh
+++ install_deps.sh
@@ -6,15 +6,7 @@ chmod -R 0766 /app/public
 chown -R node:node /app/public
 
 rm -rf /app/data
-mkdir -p /data/dumpFolder
-mkdir -p /data/logs
-mkdir -p /data/pdf
-mkdir -p /data/uploads
-mkdir -p /data/zippedProjects
+mkdir -p /data/{dumpFolder,logs,pdf,uploads,zippedProjects}
 chmod -R 0766 /data/
 chown -R node:node /data/
 ln -s /data/ /app/data
-
-cat /app/node_modules/metrics-sharelatex/package.json
-
-#apt-get update && apt-get install vim -y
