e3382aa184e211b9353f54bc7de647e7ae0f7d43
Henry Oswald <henry.oswald@sharelatex.com>
2019-02-06 13:55:23 +0000

    change permissions to 0755

--- install_deps.sh
+++ install_deps.sh
@@ -4,11 +4,11 @@ make --no-print-directory lint
 
 npm install git+https://github.com/sharelatex/translations-sharelatex.git#master
 
-chmod -R 0766 /app/public
+chmod -R 0755 /app/public
 chown -R node:node /app/public
 
 rm -rf /app/data
 mkdir -p /data/{dumpFolder,logs,pdf,uploads,zippedProjects}
-chmod -R 0766 /data/
+chmod -R 0755 /data/
 chown -R node:node /data/
 ln -s /data/ /app/data
