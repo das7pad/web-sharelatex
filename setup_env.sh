5bc5e6281889de89f0e352e53c7aacf42fd247e1
Jakob Ackermann <das7pad@outlook.com>
2019-05-12 13:02:15 +0200

    Bump the dev env to 2.2.1
    Move the Code Style check out of the Docker build stage.
    
    Signed-off-by: Jakob Ackermann <das7pad@outlook.com>

--- setup_env.sh
+++ setup_env.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-make --no-print-directory format & FORMAT=$!
-make --no-print-directory lint & LINT=$!
 npm install git+https://github.com/sharelatex/translations-sharelatex.git#master & TRANSLATIONS=$!
 WEBPACK_ENV=production make minify & MINIFY=$!
 
-echo "Waiting for lint, format, translations and minify to finish"
-
-wait $LINT && echo "Lint complete" || exit 1
-wait $FORMAT && echo "Format complete" || exit 1
 wait $TRANSLATIONS && echo "Translations install complete" || exit 1
 wait $MINIFY && echo "Minifiy complete" || exit 1
 
