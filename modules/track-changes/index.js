a6f5e9805c389df0e9aee20cdc0f0bb7c0c2c5b2
decaffeinate <das7pad@outlook.com>
2019-10-19 13:05:33 +0200

    decaffeinate: Run post-processing cleanups on index.coffee

--- modules/track-changes/index.js
+++ modules/track-changes/index.js
@@ -1,3 +1,8 @@
+/* eslint-disable
+    no-unused-vars,
+*/
+// TODO: This file was created by bulk-decaffeinate.
+// Fix any style issues and re-enable lint.
 let TrackChanges;
 const TrackChangesRouter = require("./app/js/TrackChangesRouter");
 const logger = require("logger-sharelatex");
