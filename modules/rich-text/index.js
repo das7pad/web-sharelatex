872c8498b15036bbc9c3da7fddd635b867aa7472
decaffeinate <das7pad@outlook.com>
2019-10-19 13:05:30 +0200

    decaffeinate: Run post-processing cleanups on index.coffee

--- modules/rich-text/index.js
+++ modules/rich-text/index.js
@@ -1,3 +1,8 @@
+/* eslint-disable
+    no-unused-vars,
+*/
+// TODO: This file was created by bulk-decaffeinate.
+// Fix any style issues and re-enable lint.
 const Features = require('../../app/js/infrastructure/Features');
 
 module.exports = {
