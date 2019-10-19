e19af6dedb845cec6efb0c2c343905a36c9a4b10
decaffeinate <das7pad@outlook.com>
2019-10-19 13:05:25 +0200

    decaffeinate: Convert index.coffee to JS

--- modules/public-registration/index.js
+++ modules/public-registration/index.js
@@ -1,10 +1,11 @@
-PublicRegistrationRouter = require "./app/js/PublicRegistrationRouter"
-Features = require "../../app/js/infrastructure/Features"
+const PublicRegistrationRouter = require("./app/js/PublicRegistrationRouter");
+const Features = require("../../app/js/infrastructure/Features");
 
-PublicRegistrationModule =
-	router: PublicRegistrationRouter
+const PublicRegistrationModule =
+	{router: PublicRegistrationRouter};
 
-if Features.hasFeature('registration')
-	module.exports = PublicRegistrationModule
-else
-	module.exports = {}
+if (Features.hasFeature('registration')) {
+	module.exports = PublicRegistrationModule;
+} else {
+	module.exports = {};
+}
