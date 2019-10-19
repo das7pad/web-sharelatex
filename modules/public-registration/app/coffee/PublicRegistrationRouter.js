0583aec1135c9fe9bc36097896579b7c899622c4
decaffeinate <das7pad@outlook.com>
2019-10-19 13:23:20 +0200

    decaffeinate: Convert PublicRegistrationController.coffee and 1 other file to JS

--- modules/public-registration/app/coffee/PublicRegistrationRouter.js
+++ modules/public-registration/app/coffee/PublicRegistrationRouter.js
@@ -1,28 +1,47 @@
-PublicRegistrationController = require("./PublicRegistrationController")
-AuthenticationController = require('../../../../app/js/Features/Authentication/AuthenticationController')
-CaptchaMiddleware = require '../../../../app/js/Features/Captcha/CaptchaMiddleware'
-Settings = require 'settings-sharelatex'
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * DS103: Rewrite code to no longer use __guard__
+ * DS207: Consider shorter variations of null checks
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+const PublicRegistrationController = require("./PublicRegistrationController");
+const AuthenticationController = require('../../../../app/js/Features/Authentication/AuthenticationController');
+const CaptchaMiddleware = require('../../../../app/js/Features/Captcha/CaptchaMiddleware');
+const Settings = require('settings-sharelatex');
 
-logger = require("logger-sharelatex")
+const logger = require("logger-sharelatex");
 
-module.exports = 
-	apply: (webRouter) ->
-		# This registration module has been superceeded by the work in
-		# Overleaf integration, but we're not yet ready to rip this out,
-		# because this is entangled with the acceptance tests.
-		#
-		# This option will be on for test runs
-		if Settings.enableLegacyRegistration
-			removeRoute webRouter, "get", "/register"
-			webRouter.get "/register", PublicRegistrationController.showRegisterPage
-			webRouter.post "/register", CaptchaMiddleware.validateCaptcha('register'), PublicRegistrationController.register
-			AuthenticationController.addEndpointToLoginWhitelist '/register'
+module.exports = { 
+	apply(webRouter) {
+		// This registration module has been superceeded by the work in
+		// Overleaf integration, but we're not yet ready to rip this out,
+		// because this is entangled with the acceptance tests.
+		//
+		// This option will be on for test runs
+		if (Settings.enableLegacyRegistration) {
+			removeRoute(webRouter, "get", "/register");
+			webRouter.get("/register", PublicRegistrationController.showRegisterPage);
+			webRouter.post("/register", CaptchaMiddleware.validateCaptcha('register'), PublicRegistrationController.register);
+			return AuthenticationController.addEndpointToLoginWhitelist('/register');
+		}
+	}
+};
 
-removeRoute = (webRouter, method, path)->
-	index = null
-	for route, i in webRouter.stack
-		if route?.route?.path == path
-			index = i
-	if index?
-		logger.log method:method, path:path, index:index, "removing route from express router"
-		webRouter.stack.splice(index,1)
+var removeRoute = function(webRouter, method, path){
+	let index = null;
+	for (let i = 0; i < webRouter.stack.length; i++) {
+		const route = webRouter.stack[i];
+		if (__guard__(route != null ? route.route : undefined, x => x.path) === path) {
+			index = i;
+		}
+	}
+	if (index != null) {
+		logger.log({method, path, index}, "removing route from express router");
+		return webRouter.stack.splice(index,1);
+	}
+};
+
+function __guard__(value, transform) {
+  return (typeof value !== 'undefined' && value !== null) ? transform(value) : undefined;
+}
