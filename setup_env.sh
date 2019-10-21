87ef39a199da411af70a342ed04eab4d2e37d5f4
Jakob Ackermann <das7pad@outlook.com>
2019-10-21 12:41:49 +0200

    [misc] monkey patch the migrations in CI
    Signed-off-by: Jakob Ackermann <das7pad@outlook.com>

--- setup_env.sh
+++ setup_env.sh
@@ -2,6 +2,9 @@
 
 set -B
 
+# monkey patch: east want to create a `.migrations` file
+chown node:node /app/migrations
+
 rm -rf /app/data
 mkdir -p /app/data/{dumpFolder,logs,pdf,uploads,zippedProjects}
 chmod -R 0755 /app/data/
