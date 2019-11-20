15b6b0d2eb59408a4487ae7f5d4010b97ad5d6aa
Jakob Ackermann <das7pad@outlook.com>
2019-11-20 12:54:12 +0100

    [SmokeTests] rewrite the IPC with plain JavaScript
    With a hook into the connection creation of the request package we can
     trick it to use our custom domain name without setting e.g. an entry
     in /etc/hosts or actually resolving the domain name - this improves
     performance too, as dns lookups block one of the IO worker threads.
    
    With a hook into the cookie propagation we can degrade the security
     of the cookies for the use with plain http in a non browser context.
    All permutations of secureCookie 1/0, cookieDomain set/undefined,
     behindProxy 1/0 have been tested. All but secureCookie=1 behindProxy=0
     passed with success. Which is expected as the faked https protocol
     via a header is only detected with the proxy flag enabled and without
     https there is no such thing as secure communication, hence no cookies
     are set by express.
    
    Some of the done callbacks may have been invoked twice due to missing
     return statements in the error handling, this has been fixed.
    
    A failed login request was not detected previously, resulting in verbose
     results for the following failing test cases. A failure can be detected
     with a check on the response payload, but not the response status code,
     which is 200 in most cases. From the frontend code I took the check
       body.message.type === 'error'
    
    A quick benchmark with a local mongodb, local redis-server and a 3.4Ghz
     CPU produced these results from 200 requests via ab:
      $ ab -n 200 127.0.0.1:3000/health_check
    
                     min  mean[+/-sd] median   max  99%
     curl IPC:       316  394  45.4    419     462  458
     JavaScript IPC: 316  322   5.9    321     347  346
    
    Signed-off-by: Jakob Ackermann <das7pad@outlook.com>

--- test/smoke/src/SmokeTests.js
+++ test/smoke/src/SmokeTests.js
@@ -1,57 +1,38 @@
-/* eslint-disable
-    max-len,
-    no-unused-vars,
-    no-useless-escape,
-*/
-// TODO: This file was created by bulk-decaffeinate.
-// Fix any style issues and re-enable lint.
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-const child = require('child_process')
-let fs = require('fs')
-const assert = require('assert')
 const chai = require('chai')
-if (Object.prototype.should == null) {
-  chai.should()
-}
 const { expect } = chai
 const Settings = require('settings-sharelatex')
 let ownPort = Settings.internal.web.port || Settings.port || 3000
 const port = Settings.web.web_router_port || ownPort // send requests to web router if this is the api process
-const cookeFilePath = `/tmp/smoke-test-cookie-${ownPort}-to-${port}.txt`
-const buildUrl = path =>
-  ` -b ${cookeFilePath} --resolve 'smoke${
-    Settings.cookieDomain
-  }:${port}:127.0.0.1' http://smoke${
-    Settings.cookieDomain
-  }:${port}/${path}?setLng=en`
 const logger = require('logger-sharelatex')
 const LoginRateLimiter = require('../../../app/src/Features/Security/LoginRateLimiter')
 const RateLimiter = require('../../../app/src/infrastructure/RateLimiter')
 
-// Change cookie to be non secure so curl will send it
-const convertCookieFile = function(callback) {
-  fs = require('fs')
-  return fs.readFile(cookeFilePath, 'utf8', (err, data) => {
-    if (err) {
-      return callback(err)
-    }
-    const firstTrue = data.indexOf('TRUE')
-    const secondTrue = data.indexOf('TRUE', firstTrue + 4)
-    const result =
-      data.slice(0, secondTrue) + 'FALSE' + data.slice(secondTrue + 4)
-    return fs.writeFile(cookeFilePath, result, 'utf8', err => {
-      if (err) {
-        return callback(err)
-      }
-      return callback()
-    })
-  })
+const agent = require('http').Agent()
+// like the curl option `--resolve DOMAIN:PORT:127.0.0.1`
+agent.createConnection = function alwaysConnectToLocalhost(options, callback) {
+  return require('net').createConnection(port, '127.0.0.1', callback)
 }
+
+const jar = require('request').jar()
+const actualJarSetCookie = jar.setCookie
+jar.setCookie = function degradeCookieSecurity() {
+  const cookie = actualJarSetCookie.apply(this, arguments)
+  cookie.secure = false
+  cookie.httpOnly = false
+  return cookie
+}
+
+const request = require('request').defaults({
+  baseUrl: `http://smoke${Settings.cookieDomain}/`,
+  jar: jar,
+  agent: agent,
+  headers: {
+    'X-Forwarded-Proto': 'https'
+  },
+  qs: {
+    setLng: 'en'
+  }
+})
 
 describe('Opening', function() {
   before(function(done) {
@@ -93,125 +74,85 @@ describe('Opening', function() {
 
   before(function(done) {
     logger.log('smoke test: hitting dev/csrf')
-    let command = `\
-curl -H  "X-Forwarded-Proto: https" -c ${cookeFilePath} ${buildUrl('dev/csrf')}\
-`
-    child.exec(command, (err, stdout, stderr) => {
+    request.get('dev/csrf', {}, (err, response, body) => {
       if (err != null) {
-        done(err)
+        return done(err)
       }
-      const csrf = stdout
-      logger.log('smoke test: converting cookie file 1')
-      return convertCookieFile(err => {
+      logger.log('smoke test: hitting /login with csrf')
+      const json = {
+        _csrf: body,
+        email: Settings.smokeTest.user,
+        password: Settings.smokeTest.password
+      }
+      request.post('login', { json }, (err, response, body) => {
         if (err != null) {
           return done(err)
         }
-        logger.log('smoke test: hitting /login with csrf')
-        command = `\
-curl -c ${cookeFilePath} -H "Content-Type: application/json" -H "X-Forwarded-Proto: https" -d '{"_csrf":"${csrf}", "email":"${
-          Settings.smokeTest.user
-        }", "password":"${Settings.smokeTest.password}"}' ${buildUrl('login')}\
-`
-        return child.exec(command, err => {
-          if (err != null) {
-            return done(err)
-          }
-          logger.log('smoke test: finishing setup')
-          return convertCookieFile(done)
-        })
+        // login success and login failure both receive a status code of 200
+        // see the frontend logic on how to handle the response:
+        //   frontend/js/directives/asyncForm.js -> submitRequest
+        if (body && body.message && body.message.type === 'error') {
+          const text = body.message.text
+          logger.err({ text }, 'smoke test: login failed')
+          return done(new Error(`Login failed: ${text}`))
+        }
+        logger.log('smoke test: finishing setup')
+        done()
       })
     })
   })
 
   after(function(done) {
-    logger.log('smoke test: converting cookie file 2')
-    convertCookieFile(err => {
+    logger.log('smoke test: cleaning up')
+    request.get('dev/csrf', {}, (err, response, body) => {
       if (err != null) {
         return done(err)
       }
-      logger.log('smoke test: cleaning up')
-      let command = `\
-curl -H  "X-Forwarded-Proto: https" -c ${cookeFilePath} ${buildUrl('dev/csrf')}\
-`
-      return child.exec(command, (err, stdout, stderr) => {
-        if (err != null) {
-          done(err)
-        }
-        const csrf = stdout
-        logger.log('smoke test: converting cookie file 3')
-        return convertCookieFile(err => {
-          if (err != null) {
-            return done(err)
-          }
-          command = `\
-curl -H "Content-Type: application/json" -H "X-Forwarded-Proto: https" -d '{"_csrf":"${csrf}"}' -c ${cookeFilePath} ${buildUrl(
-            'logout'
-          )}\
-`
-          return child.exec(command, (err, stdout, stderr) => {
-            if (err != null) {
-              return done(err)
-            }
-            return fs.unlink(cookeFilePath, done)
-          })
-        })
-      })
+      const json = { _csrf: body }
+      request.post('logout', { json }, done)
     })
   })
 
   it('a project', function(done) {
     logger.log('smoke test: Checking can load a project')
     this.timeout(4000)
-    const command = `\
-curl -H "X-Forwarded-Proto: https" -v ${buildUrl(
-      `project/${Settings.smokeTest.projectId}`
-    )}\
-`
-    return child.exec(command, (error, stdout, stderr) => {
+    const uri = `project/${Settings.smokeTest.projectId}`
+    request.get(uri, {}, (error, response, body) => {
       expect(error, 'smoke test: error in getting project').to.not.exist
 
-      const statusCodeMatch = !!stderr.match('200 OK')
-      expect(
-        statusCodeMatch,
+      expect(response.statusCode).to.equal(
+        200,
         'smoke test: response code is not 200 getting project'
-      ).to.equal(true)
+      )
 
       // Check that the project id is present in the javascript that loads up the project
-      const match = !!stdout.match(
-        `window.project_id = \"${Settings.smokeTest.projectId}\"`
-      )
-      expect(
-        match,
+      expect(body).to.include(
+        `window.project_id = "${Settings.smokeTest.projectId}"`,
         'smoke test: project page html does not have project_id'
-      ).to.equal(true)
-      return done()
+      )
+      done()
     })
   })
 
   it('the project list', function(done) {
     logger.log('smoke test: Checking can load project list')
     this.timeout(4000)
-    const command = `\
-curl -H "X-Forwarded-Proto: https" -v ${buildUrl('project')}\
-`
-    return child.exec(command, (error, stdout, stderr) => {
+    request.get('project', {}, (error, response, body) => {
       expect(error, 'smoke test: error returned in getting project list').to.not
         .exist
-      expect(
-        !!stderr.match('200 OK'),
+      expect(response.statusCode).to.equal(
+        200,
         'smoke test: response code is not 200 getting project list'
-      ).to.equal(true)
-      expect(
-        !!stdout.match(
-          '<title>Your Projects - .*, Online LaTeX Editor</title>'
-        ),
+      )
+      expect(body).to.match(
+        /<title>Your Projects - .*, Online LaTeX Editor<\/title>/,
         'smoke test: body does not have correct title'
-      ).to.equal(true)
-      expect(
-        !!stdout.match('ProjectPageController'),
+      )
+      expect(body).to.include(
+        'ProjectPageController',
         'smoke test: body does not have correct angular controller'
-      ).to.equal(true)
-      return done()
+      )
+      done()
     })
   })
 })
