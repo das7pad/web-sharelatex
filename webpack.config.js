65f04afcf5dcf9fdec0d7a79612f7a9d8159b0e6
Jakob Ackermann <das7pad@outlook.com>
2019-12-08 11:18:23 +0100

    [webpack] fix the source map generation for css
    REF: d532b53eb0fe61119f950ed5ef816041a48562f4
    Signed-off-by: Jakob Ackermann <das7pad@outlook.com>

--- webpack.config.js
+++ webpack.config.js
@@ -110,6 +110,7 @@ module.exports = {
           {
             loader: 'postcss-loader',
             options: {
+              sourceMap: true,
               plugins: [AutoPrefixer]
             }
           },
