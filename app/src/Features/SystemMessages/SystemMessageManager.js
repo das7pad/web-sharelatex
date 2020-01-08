b989199d0c3c4d71fe432386a2e7f2353f4eaeb0
Jakob Ackermann <das7pad@outlook.com>
2020-01-08 12:45:46 +0100

    Revert "[SystemMessageManager] getMessages: caching: queue concurrent requests"
    This reverts commit c6f6d4fb7e7123f28c74a387f9a93953f529f875
    
    Signed-off-by: Jakob Ackermann <das7pad@outlook.com>

--- app/src/Features/SystemMessages/SystemMessageManager.js
+++ app/src/Features/SystemMessages/SystemMessageManager.js
@@ -13,26 +13,21 @@ let SystemMessageManager
 const { SystemMessage } = require('../../models/SystemMessage')
 
 module.exports = SystemMessageManager = {
-  pending: [],
-
   getMessages(callback) {
     if (callback == null) {
       callback = function(error, messages) {}
     }
     if (this._cachedMessages != null) {
       return callback(null, this._cachedMessages)
-    }
-    if (this.pending.push(callback) !== 1) {
-      return
-    }
-    this.getMessagesFromDB((error, messages) => {
-      if (error == null) {
+    } else {
+      return this.getMessagesFromDB((error, messages) => {
+        if (error != null) {
+          return callback(error)
+        }
         this._cachedMessages = messages
-      }
-      const pending = this.pending
-      this.pending = []
-      pending.forEach(cb => process.nextTick(cb, error, messages))
-    })
+        return callback(null, messages)
+      })
+    }
   },
 
   getMessagesFromDB(callback) {
