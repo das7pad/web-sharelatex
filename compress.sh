35fb0e192811de52334112a99737eb8d67b58a1e
Jakob Ackermann <das7pad@outlook.com>
2020-02-16 15:59:45 +0100

    [compress] normalize modified times of all files
    Signed-off-by: Jakob Ackermann <das7pad@outlook.com>

--- compress.sh
+++ compress.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 FILES=$(find -type f -not -name '*.gz')
+NORMALIZE_LAST_MODIFIED="touch -t 202001010000 -m"
+
+# normalize in order to get reproducible tar balls
+${NORMALIZE_LAST_MODIFIED} ${FILES}
 
 for file in ${FILES}; do
     dest=${file}.gz
@@ -15,3 +19,6 @@ for file in ${FILES}; do
     gzip -9 --stdout ${file} > ${dest}
     touch -m --reference ${file} ${dest}
 done
+
+# normalize in order to get reproducible ETags (based on m-time)
+find . -name '*.gz' -exec ${NORMALIZE_LAST_MODIFIED} {} +
