6e925c061ff68cdb5f98f1e12dd0b38aa58578ca
Jakob Ackermann <das7pad@outlook.com>
2020-04-12 02:13:24 +0200

    [misc] open-in-overleaf: tests: inherit the migrations and db cleanup

--- modules/open-in-overleaf/test/acceptance/src/Init.js
+++ modules/open-in-overleaf/test/acceptance/src/Init.js
@@ -1,13 +1,1 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-const App = require('../../../../../app')
-
-before(function(done) {
-  this.timeout(20000)
-  return App.listen(3000, 'localhost', done)
-})
+module.exports = require('../../../../../test/acceptance/src/Init')
