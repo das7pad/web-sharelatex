22e05937aca7590bdf5bdbdce8ec44164c7cbe7b
Jakob Ackermann <das7pad@outlook.com>
2020-05-23 16:42:13 +0200

    [misc] convert pdfjsBundle to esm
    Signed-off-by: Jakob Ackermann <das7pad@outlook.com>

--- frontend/js/pdfjsBundle.js
+++ frontend/js/pdfjsBundle.js
@@ -1,18 +1,20 @@
 /*
   Inject the worker path and load the worker with a low priority
  */
-define(['pdfjs-dist/build/pdf.js'], function(pdfjsBundle) {
-  pdfjsBundle.GlobalWorkerOptions.workerSrc = `${window.staticPath}vendor/pdfjs-dist/build/pdf.worker.min.js`
-  if (typeof window !== 'undefined' && 'Worker' in window) {
-    // preload the worker with a low priority
-    const link = document.createElement('link')
-    link.rel = 'prefetch'
-    link.as = 'script'
-    link.href = pdfjsBundle.GlobalWorkerOptions.workerSrc
-    link.onload = () => {
-      pdfjsBundle.worker = new pdfjsBundle.PDFWorker()
-    }
-    document.head.appendChild(link)
+
+import pdfjsBundle from 'pdfjs-dist'
+
+pdfjsBundle.GlobalWorkerOptions.workerSrc = `${window.staticPath}vendor/pdfjs-dist/build/pdf.worker.min.js`
+if (typeof window !== 'undefined' && 'Worker' in window) {
+  // preload the worker with a low priority
+  const link = document.createElement('link')
+  link.rel = 'prefetch'
+  link.as = 'script'
+  link.href = pdfjsBundle.GlobalWorkerOptions.workerSrc
+  link.onload = () => {
+    pdfjsBundle.worker = new pdfjsBundle.PDFWorker()
   }
-  return pdfjsBundle
-})
+  document.head.appendChild(link)
+}
+
+export default pdfjsBundle
