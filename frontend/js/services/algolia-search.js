1e982d0281a91cc2a6dff4e914829c3803d9d507
Jakob Ackermann <das7pad@outlook.com>
2020-05-23 23:37:11 +0200

    [frontend] algolia-search: decaffeinate cleanup
    Signed-off-by: Jakob Ackermann <das7pad@outlook.com>

--- frontend/js/services/algolia-search.js
+++ frontend/js/services/algolia-search.js
@@ -1,67 +1,18 @@
-/* eslint-disable
-    max-len,
-*/
-// TODO: This file was created by bulk-decaffeinate.
-// Fix any style issues and re-enable lint.
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS103: Rewrite code to no longer use __guard__
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 import App from '../base'
 import AlgoliaSearch from 'algoliasearch'
 
 export default App.factory('algoliaSearch', function() {
   let kbIdx, wikiIdx
-  if (
-    (window.sharelatex != null ? window.sharelatex.algolia : undefined) !=
-      null &&
-    __guard__(
-      window.sharelatex.algolia != null
-        ? window.sharelatex.algolia.indexes
-        : undefined,
-      x => x.wiki
-    ) != null
-  ) {
-    const client = AlgoliaSearch(
-      window.sharelatex.algolia != null
-        ? window.sharelatex.algolia.app_id
-        : undefined,
-      window.sharelatex.algolia != null
-        ? window.sharelatex.algolia.api_key
-        : undefined
-    )
-    wikiIdx = client.initIndex(
-      __guard__(
-        window.sharelatex.algolia != null
-          ? window.sharelatex.algolia.indexes
-          : undefined,
-        x1 => x1.wiki
-      )
-    )
-    kbIdx = client.initIndex(
-      __guard__(
-        window.sharelatex.algolia != null
-          ? window.sharelatex.algolia.indexes
-          : undefined,
-        x2 => x2.kb
-      )
-    )
+  const algoliaConfig = window.sharelatex && window.sharelatex.algolia
+  if (algoliaConfig && algoliaConfig.indexes && algoliaConfig.indexes.wiki) {
+    const client = AlgoliaSearch(algoliaConfig.app_id, algoliaConfig.api_key)
+    wikiIdx = client.initIndex(algoliaConfig.indexes.wiki)
+    kbIdx = client.initIndex(algoliaConfig.indexes.kb)
   }
 
   // searchKB is deprecated
-  const service = {
+  return {
     searchWiki: wikiIdx ? wikiIdx.search.bind(wikiIdx) : null,
     searchKB: kbIdx ? kbIdx.search.bind(kbIdx) : null
   }
-
-  return service
 })
-
-function __guard__(value, transform) {
-  return typeof value !== 'undefined' && value !== null
-    ? transform(value)
-    : undefined
-}
