7d094be789f2240794017ca3306200a9e5a837b7
Jakob Ackermann <das7pad@outlook.com>
2020-05-24 21:17:55 +0200

    [misc] move the ga event enrichment for bonus links into the frontend js
    Signed-off-by: Jakob Ackermann <das7pad@outlook.com>

--- frontend/js/main/bonus.js
+++ frontend/js/main/bonus.js
@@ -9,15 +9,28 @@
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
 import App from '../base'
-App.controller(
-  'BonusLinksController',
-  ($scope, $modal) =>
-    ($scope.openLinkToUsModal = () =>
-      $modal.open({
-        templateUrl: 'BonusLinkToUsModal',
-        controller: 'BonusModalController'
-      }))
-)
+App.controller('BonusLinksController', ($scope, $modal) => {
+  $scope.openLinkToUsModal = () =>
+    $modal.open({
+      templateUrl: 'BonusLinkToUsModal',
+      controller: 'BonusModalController'
+    })
+
+  $(function() {
+    $('.twitter').click(function() {
+      ga('send', 'event', 'referal-button', 'clicked', 'twitter')
+    })
+    $('.email').click(function() {
+      ga('send', 'event', 'referal-button', 'clicked', 'email')
+    })
+    $('.facebook').click(function() {
+      ga('send', 'event', 'referal-button', 'clicked', 'facebook')
+    })
+    $('.link').click(function() {
+      ga('send', 'event', 'referal-button', 'clicked', 'direct-link')
+    })
+  })
+})
 
 export default App.controller(
   'BonusModalController',
