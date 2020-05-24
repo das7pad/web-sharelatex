8bc10a791ef2e64d79417dcb6ceb66394d4df04d
Jakob Ackermann <das7pad@outlook.com>
2020-05-24 21:13:50 +0200

    [misc] move the ga event submission for payment_form into the frontend
    Signed-off-by: Jakob Ackermann <das7pad@outlook.com>

--- frontend/js/main/new-subscription.js
+++ frontend/js/main/new-subscription.js
@@ -29,6 +29,8 @@ export default App.controller('NewSubscriptionController', function(
   $scope.availableCurrencies = {}
   $scope.planCode = window.plan_code
 
+  eventTracking.send('pageview', 'payment_form', $scope.planCode)
+
   $scope.switchToStudent = function() {
     const currentPlanCode = window.plan_code
     const planCode = currentPlanCode.replace('collaborator', 'student')
