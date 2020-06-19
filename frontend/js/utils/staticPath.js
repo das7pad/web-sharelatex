27d4394b901ada0a44d5a45366f7d0a00b7bcb48
Jakob Ackermann <das7pad@outlook.com>
2020-06-20 00:55:06 +0200

    [misc] frontend: drop staticPath fallback logic
    Signed-off-by: Jakob Ackermann <das7pad@outlook.com>

--- frontend/js/utils/staticPath.js
+++ frontend/js/utils/staticPath.js
@@ -1,15 +1,6 @@
 import meta from './meta'
 
-const base = meta(
-  'ol-staticPath',
-  document.currentScript.src.split('/').reduceRight((url, component) => {
-    if (component !== 'js' && !url) return ''
-    if (component === 'js' && !url) return '/'
-    // start concatenating as soon as we passed the 'js/' component
-    if (url === '/') return component + '/'
-    return component + '/' + url
-  }, '')
-)
+const base = meta('ol-staticPath', '/')
 
 export default function staticPath(path) {
   return base + path
