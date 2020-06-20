4e56be1e8915e6839fa858632ca4d560642bfcf4
Jakob Ackermann <das7pad@outlook.com>
2020-06-20 16:32:16 +0200

    [misc] admin: refactor script usage as data transport to frontend
    Signed-off-by: Jakob Ackermann <das7pad@outlook.com>

--- modules/admin-panel/frontend/js/main/controllers/AdminProjectController.js
+++ modules/admin-panel/frontend/js/main/controllers/AdminProjectController.js
@@ -1,7 +1,7 @@
 import App from '../../../../../../frontend/js/base'
+import getMeta from '../../../../../../frontend/js/utils/meta'
 
 export default App.controller('AdminProjectController', function($scope) {
-  const { project } = JSON.parse($('#admin-project-data').text())
-  $scope.project = project
+  $scope.project = getMeta('ol-admin-project-data')
   $scope.editingBrandVariationId = false
 })
