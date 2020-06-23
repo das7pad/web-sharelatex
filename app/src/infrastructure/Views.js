cd34a0fd25afdcf8012b603e524834e3025c886a
Jakob Ackermann <das7pad@outlook.com>
2020-06-23 23:43:43 +0200

    [misc] remove pug template optimization -- slowdown by about 0.003
    ```
    # local mongo+redis
    $ wrk-20.04 -R 100 -d120 -t1 http://127.0.0.1:3000/project/... \
        -H 'Cookie: ...'
    ...
      12001 requests
    ...
    ```
    
    Total compile time (recorded via console.time() and accumulated after
     the run): 20451ms (with optimization) vs 20517ms (with this patch)
    
    Signed-off-by: Jakob Ackermann <das7pad@outlook.com>

--- app/src/infrastructure/Views.js
+++ app/src/infrastructure/Views.js
@@ -26,10 +26,6 @@ const viewList = globby
     return !/^_/.test(x)
   })
 
-const GLOBALS_USED_IN_PUG = ['Object', 'JSON']
-const PUG_IIFE_START = ';var locals_for_with = (locals || {});(function ('
-const PUG_IIFE_CALL = '}.call(this,'
-const PUG_TEMPLATE_END = '\\u003E";'
 const GENERATED_VIEWS = Path.join(__dirname, '../../../generated/views')
 
 function compileFile(filename) {
@@ -44,54 +40,10 @@ const templates = new Map()
 module.exports = {
   _generateTemplateModule(view) {
     const filename = view + '.pug'
-    const blob = compileFile(filename).toString()
-
-    // blob contains the template wrapped in https://www.npmjs.com/package/with
-    // 'with' basically allows a mixed usage of locals and globals.
-    // ------------------------------------------------------------------------
-    // function template(locals) {\
-    // var pug_html = "", pug_mixins = {}, pug_interp;\
-    // ;var locals_for_with = (locals || {});(function (buildCssPath, ...\
-    // , settings) {pug_html = "...";
-    // pug_html = pug_html+"...body\u003E\u003C\u002Fhtml\u003E";}.call(this,\
-    // "buildCssPath" in locals_for_with?locals_for_with.buildCssPath:\
-    // typeof buildCssPath!=="undefined"?buildCssPath:undefined,\
-    // ...\
-    // "settings" in locals_for_with?locals_for_with.settings:\
-    // typeof settings!=="undefined"?settings:undefined));;return pug_html;}
-    // ------------------------------------------------------------------------
-    // In production we do not need the logic of global lookups.
-    // We will break the lookup down to a simple object unpack (Preserved;New).
-    // ------------------------------------------------------------------------
-    // function template(locals) {\                                   P
-    // var pug_html = "", pug_mixins = {}, pug_interp;                P
-    // let {buildCssPath,...,settings} = locals                       N
-    // pug_html = "...";                                              P
-    // pug_html = pug_html+"...body\u003E\u003C\u002Fhtml\u003E";     P
-    // return pug_html}                                               N
-    // ------------------------------------------------------------------------
-    // Manipulating the function body is much simpler than replicating/stubbing
-    //  pug internals to get the designated output right away.
-
-    const withStart = blob.indexOf(PUG_IIFE_START)
-    const varsStart = withStart + PUG_IIFE_START.length
-    const varsEnd = blob.indexOf(')', varsStart)
-    const vars = blob.slice(varsStart, varsEnd).split(', ')
-    const localVars = vars.filter(
-      varName => !GLOBALS_USED_IN_PUG.includes(varName)
+    return (
+      "const pug = require('pug-runtime');module.exports = template;" +
+      compileFile(filename).toString()
     )
-
-    const templateBodyStart = varsEnd + ') {'.length
-    const templateBodyEnd =
-      blob.indexOf(PUG_TEMPLATE_END + PUG_IIFE_CALL) + PUG_TEMPLATE_END.length
-
-    return `\
-const pug = require('pug-runtime')
-module.exports = template
-${blob.slice(0, withStart)}
-let {${localVars.join(',')}} = locals
-${blob.slice(templateBodyStart, templateBodyEnd)}
-return pug_html}`
   },
   _persistGeneratedTemplate(view) {
     const body = this._generateTemplateModule(view)
