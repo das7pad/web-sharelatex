9b068426cc42ee178400d3936794b741c945a2ae
Jakob Ackermann <das7pad@outlook.com>
2020-08-21 01:13:20 +0200

    [misc] add /health_check/api route, running to the redis+mongo check

--- app/src/router.js
+++ app/src/router.js
@@ -999,6 +999,16 @@ function initialize(webRouter, privateApiRouter, publicApiRouter) {
     HealthCheckController.checkActiveHandles,
     HealthCheckController.checkApi
   )
+  publicApiRouter.get(
+    '/health_check/api',
+    HealthCheckController.checkActiveHandles,
+    HealthCheckController.checkApi
+  )
+  privateApiRouter.get(
+    '/health_check/api',
+    HealthCheckController.checkActiveHandles,
+    HealthCheckController.checkApi
+  )
 
   publicApiRouter.get('/health_check/redis', HealthCheckController.checkRedis)
   privateApiRouter.get('/health_check/redis', HealthCheckController.checkRedis)
