1617886fc49b3e06d6198a910b4bdf2678b5618a
Jakob Ackermann <das7pad@outlook.com>
2020-08-29 18:42:13 +0200

    [misc] frontend: outline: fix localStorage usage in fork

--- frontend/js/ide/outline/components/OutlinePane.js
+++ frontend/js/ide/outline/components/OutlinePane.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { OverlayTrigger, Tooltip } from 'react-bootstrap'
 import classNames from 'classnames'
 import OutlineRoot from './OutlineRoot'
-import localStorage from '../../../modules/localStorage'
+import { localStorage } from '../../../modules/storage'
 
 function OutlinePane({ isTexFile, outline, projectId, jumpToLine, onToggle }) {
   const storageKey = `file_outline.expanded.${projectId}`
