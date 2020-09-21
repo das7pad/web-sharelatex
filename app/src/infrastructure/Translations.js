302bceb23ed4c2b47609248dc2abefcd7116514b
Jakob Ackermann <das7pad@outlook.com>
2020-09-21 21:56:59 +0200

    [misc] Translations: deindent Translations.setup()

--- app/src/infrastructure/Translations.js
+++ app/src/infrastructure/Translations.js
@@ -13,93 +13,89 @@ function loadAll(availableLngCodes = []) {
   return new Map(availableLngCodes.map(lng => [lng, getModule(lng)]))
 }
 
-const Translations = {
-  setup(options) {
-    options = options || {}
-    const subdomainLang = new Map(Object.entries(options.subdomainLang || {}))
-    const availableLngCodes = []
-    const availableHosts = new Map()
-    subdomainLang.forEach(function(spec) {
-      availableLngCodes.push(spec.lngCode)
-      availableHosts.set(new URL(spec.url).host, spec.lngCode)
-    })
+const options = Settings.i18n || {}
 
-    const fallbackLng = options.defaultLng || 'en'
-    if (!availableLngCodes.includes(fallbackLng)) {
-      availableLngCodes.push(fallbackLng)
-    }
-    const allLocales = loadAll(availableLngCodes)
+const subdomainLang = new Map(Object.entries(options.subdomainLang || {}))
+const availableLngCodes = []
+const availableHosts = new Map()
+subdomainLang.forEach(function(spec) {
+  availableLngCodes.push(spec.lngCode)
+  availableHosts.set(new URL(spec.url).host, spec.lngCode)
+})
 
-    function setLangBasedOnDomainMiddleware(req, res, next) {
-      res.locals.getTranslationUrl = spec => {
-        return spec.url + req.originalUrl
-      }
+const fallbackLng = options.defaultLng || 'en'
+if (!availableLngCodes.includes(fallbackLng)) {
+  availableLngCodes.push(fallbackLng)
+}
+const allLocales = loadAll(availableLngCodes)
 
-      // prefer host and then fallback language over browser hint
-      req.language = availableHosts.get(req.headers.host) || fallbackLng
-      postprocess(req, res, next)
-    }
-    function setLangBasedOnSessionOrQueryParam(req, res, next) {
-      res.locals.getTranslationUrl = spec => {
-        const url = new URL(spec.url + req.originalUrl)
-        // add the setGlobalLng query parameter while preserving other params
-        url.searchParams.append('setGlobalLng', spec.lngCode)
-        return url.href
-      }
-
-      if (req.query.setGlobalLng && subdomainLang.has(req.query.setGlobalLng)) {
-        const { lngCode, url } = subdomainLang.get(req.query.setGlobalLng)
-        req.session.lng = lngCode
-        // cleanup the setGlobalLng query parameter and preserve other params
-        const parsedURL = new URL(url + req.originalUrl)
-        parsedURL.searchParams.delete('setGlobalLng')
-        return res.redirect(parsedURL.href)
-      }
-
-      // prefer session and then fallback language over browser hint
-      req.language = req.session.lng || fallbackLng
-      postprocess(req, res, next)
-    }
-
-    const singleDomainMultipleLng =
-      typeof options.singleDomainMultipleLng === 'boolean'
-        ? options.singleDomainMultipleLng
-        : availableHosts.size === 1 && availableLngCodes.length !== 1
-    const middleware = singleDomainMultipleLng
-      ? setLangBasedOnSessionOrQueryParam
-      : setLangBasedOnDomainMiddleware
-
-    function postprocess(req, res, next) {
-      if (req.query.setLng) {
-        // Developers/Users can override the language per request
-        if (!availableLngCodes.includes(req.query.setLng)) {
-          return res.status(400).json({ message: 'invalid lngCode' })
-        }
-        req.language = req.query.setLng
-      }
-
-      const browserLanguage = req.acceptsLanguages(availableLngCodes)
-      if (browserLanguage && browserLanguage !== req.language) {
-        // 'accept-language' header and fallbackLng mismatch
-        // 'accept-language' header and host header mismatch
-        // 'accept-language' header and ?setGlobalLng mismatch
-        // 'accept-language' header and ?setLng mismatch
-        req.showUserOtherLng = browserLanguage
-      }
-
-      req.lng = req.locale = req.language
-      req.i18n = {}
-      req.i18n.t = req.i18n.translate = allLocales.get(req.language)
-      next()
-    }
-
-    // backwards compatibility
-    middleware.expressMiddleware = middleware.expressMiddlewear = middleware
-    middleware.setLangBasedOnDomainMiddlewear = (req, res, next) => next()
-    middleware.setLangBasedOnDomainMiddleware = (req, res, next) => next()
-    middleware.i18n = { translate: allLocales.get('en') }
-    return middleware
+function setLangBasedOnDomainMiddleware(req, res, next) {
+  res.locals.getTranslationUrl = spec => {
+    return spec.url + req.originalUrl
   }
+
+  // prefer host and then fallback language over browser hint
+  req.language = availableHosts.get(req.headers.host) || fallbackLng
+  postprocess(req, res, next)
+}
+function setLangBasedOnSessionOrQueryParam(req, res, next) {
+  res.locals.getTranslationUrl = spec => {
+    const url = new URL(spec.url + req.originalUrl)
+    // add the setGlobalLng query parameter while preserving other params
+    url.searchParams.append('setGlobalLng', spec.lngCode)
+    return url.href
+  }
+
+  if (req.query.setGlobalLng && subdomainLang.has(req.query.setGlobalLng)) {
+    const { lngCode, url } = subdomainLang.get(req.query.setGlobalLng)
+    req.session.lng = lngCode
+    // cleanup the setGlobalLng query parameter and preserve other params
+    const parsedURL = new URL(url + req.originalUrl)
+    parsedURL.searchParams.delete('setGlobalLng')
+    return res.redirect(parsedURL.href)
+  }
+
+  // prefer session and then fallback language over browser hint
+  req.language = req.session.lng || fallbackLng
+  postprocess(req, res, next)
 }
 
-module.exports = Translations.setup(Settings.i18n)
+const singleDomainMultipleLng =
+  typeof options.singleDomainMultipleLng === 'boolean'
+    ? options.singleDomainMultipleLng
+    : availableHosts.size === 1 && availableLngCodes.length !== 1
+const middleware = singleDomainMultipleLng
+  ? setLangBasedOnSessionOrQueryParam
+  : setLangBasedOnDomainMiddleware
+
+function postprocess(req, res, next) {
+  if (req.query.setLng) {
+    // Developers/Users can override the language per request
+    if (!availableLngCodes.includes(req.query.setLng)) {
+      return res.status(400).json({ message: 'invalid lngCode' })
+    }
+    req.language = req.query.setLng
+  }
+
+  const browserLanguage = req.acceptsLanguages(availableLngCodes)
+  if (browserLanguage && browserLanguage !== req.language) {
+    // 'accept-language' header and fallbackLng mismatch
+    // 'accept-language' header and host header mismatch
+    // 'accept-language' header and ?setGlobalLng mismatch
+    // 'accept-language' header and ?setLng mismatch
+    req.showUserOtherLng = browserLanguage
+  }
+
+  req.lng = req.locale = req.language
+  req.i18n = {}
+  req.i18n.t = req.i18n.translate = allLocales.get(req.language)
+  next()
+}
+
+// backwards compatibility
+middleware.expressMiddleware = middleware.expressMiddlewear = middleware
+middleware.setLangBasedOnDomainMiddlewear = (req, res, next) => next()
+middleware.setLangBasedOnDomainMiddleware = (req, res, next) => next()
+middleware.i18n = { translate: allLocales.get('en') }
+
+module.exports = middleware
