9ea60ca30665d0e0677d9abe3ba08b90c0e03cae
Jakob Ackermann <das7pad@outlook.com>
2020-10-11 13:41:57 +0200

    [misc] use es6 imports for modules/modules-TYPE.js dependencies

--- modules/modules-main.js
+++ modules/modules-main.js
@@ -1,9 +1,8 @@
-const deps = require('glob').sync('./*/frontend/js/main/index.js', {
-  cwd: __dirname
-})
-
 module.exports = function() {
   return {
-    code: `define(${JSON.stringify(deps)}, function() {})`
+    code: require('glob')
+      .sync('./*/frontend/js/main/index.js', { cwd: __dirname })
+      .map(file => `import '${file}'`)
+      .join('\n')
   }
 }
