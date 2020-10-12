b6c452baddae7fbd23fa26f856c8e1ceb8782a33
Jakob Ackermann <das7pad@outlook.com>
2020-10-13 00:00:38 +0200

    [misc] Views: move function definitions out of module.exports object

--- app/src/infrastructure/Views.js
+++ app/src/infrastructure/Views.js
@@ -18,41 +18,47 @@ function getBuiltViewList() {
 }
 
 const preloadedTemplates = new Map()
-module.exports = {
-  generateTemplate(path) {
-    // block compiles at runtime when there are existing preloaded templates
-    if (preloadedTemplates.size) {
-      throw new Error(`template not preloaded: ${path}`)
-    }
-    // Load the generator ad-hoc in development.
-    // In production we do not ship 'pug', but just 'pug-runtime'.
-    return require('../../../build/views/generator').generateModuleInMemory({
-      path,
-      cache: false,
-      debug: Settings.debugPugTemplates
-    })
-  },
-  getTemplate(path) {
-    return preloadedTemplates.get(path) || this.generateTemplate(path)
-  },
-  loadPrecompiledViews(app) {
-    app.engines['.pug'] = function render(path, locals, callback) {
-      const template = preloadedTemplates.get(path)
-      // express already has a try/catch wrapper around this invocation.
-      // There is no need for any error handling in here.
-      callback(null, template(locals))
-    }
-
-    getBuiltViewList().forEach(({ pugViewPath, builtViewPath }) => {
-      preloadedTemplates.set(pugViewPath, require(builtViewPath))
-    })
-  },
-
-  precompileViews(app) {
-    require('../../../build/views').processViews({
-      write: false,
-      cache: true,
-      debug: Settings.debugPugTemplates
-    })
+function generateTemplate(path) {
+  // block compiles at runtime when there are existing preloaded templates
+  if (preloadedTemplates.size) {
+    throw new Error(`template not preloaded: ${path}`)
   }
+  // Load the generator ad-hoc in development.
+  // In production we do not ship 'pug', but just 'pug-runtime'.
+  return require('../../../build/views/generator').generateModuleInMemory({
+    path,
+    cache: false,
+    debug: Settings.debugPugTemplates
+  })
+}
+
+function getTemplate(path) {
+  return preloadedTemplates.get(path) || generateTemplate(path)
+}
+
+function loadPrecompiledViews(app) {
+  app.engines['.pug'] = function render(path, locals, callback) {
+    const template = preloadedTemplates.get(path)
+    // express already has a try/catch wrapper around this invocation.
+    // There is no need for any error handling in here.
+    callback(null, template(locals))
+  }
+
+  getBuiltViewList().forEach(({ pugViewPath, builtViewPath }) => {
+    preloadedTemplates.set(pugViewPath, require(builtViewPath))
+  })
+}
+
+function precompileViews() {
+  require('../../../build/views').processViews({
+    write: false,
+    cache: true,
+    debug: Settings.debugPugTemplates
+  })
+}
+
+module.exports = {
+  getTemplate,
+  loadPrecompiledViews,
+  precompileViews
 }
