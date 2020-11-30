f8394b4ad004ff3bf391a8a7620863194916a8ac
Jakob Ackermann <das7pad@outlook.com>
2020-11-30 23:57:06 +0100

    [misc] decaffeinate acceptance test settings 2/2

--- modules/open-in-overleaf/test/acceptance/config/settings.test.js
+++ modules/open-in-overleaf/test/acceptance/config/settings.test.js
@@ -1,60 +1,80 @@
-module.exports =
-	enableLegacyRegistration: true
-	enableLegacyLogin: false
-	apis:
-		analytics:
-			enabled: false
-			url: undefined
-		v1:
-			url: "http://#{process.env['V1_HOST'] || 'localhost'}:5000"
-			user: 'overleaf'
-			pass: 'password'
-		linkedUrlProxy:
-			url: "http://#{process.env['LINKED_URL_PROXY_HOST'] || 'localhost'}:6543"
+module.exports = {
+  enableLegacyRegistration: true,
+  enableLegacyLogin: false,
+  apis: {
+    analytics: {
+      enabled: false,
+      url: undefined
+    },
+    v1: {
+      url: `http://${process.env['V1_HOST'] || 'localhost'}:5000`,
+      user: 'overleaf',
+      pass: 'password'
+    },
+    linkedUrlProxy: {
+      url: `http://${process.env['LINKED_URL_PROXY_HOST'] || 'localhost'}:6543`
+    }
+  },
 
-	collabratec:
-		oauth:
-			client_id: "mock-collabratec-oauth-client-id"
-		saml:
-			callback_host: "http://mock-callback-host.com"
-			callback_path: "/org/ieee/saml/consume"
-			entry_point: "http://mock-entry-point.com"
-			identifier_format: "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified"
-			init_path: "/org/ieee/saml/init"
-			issuer: "mock-issuer"
-	overleaf:
-		host: "http://#{process.env['V1_HOST'] || 'localhost'}:5000"
-		s3:
-			host: "http://localhost:5001" # MockS3Api
-		oauth:
-			clientID: "mock-oauth-client-id"
-			clientSecret: "mock-oauth-client-secret"
+  collabratec: {
+    oauth: {
+      client_id: 'mock-collabratec-oauth-client-id'
+    },
+    saml: {
+      callback_host: 'http://mock-callback-host.com',
+      callback_path: '/org/ieee/saml/consume',
+      entry_point: 'http://mock-entry-point.com',
+      identifier_format:
+        'urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified',
+      init_path: '/org/ieee/saml/init',
+      issuer: 'mock-issuer'
+    }
+  },
+  overleaf: {
+    host: `http://${process.env['V1_HOST'] || 'localhost'}:5000`,
+    s3: {
+      host: 'http://localhost:5001'
+    }, // MockS3Api
+    oauth: {
+      clientID: 'mock-oauth-client-id',
+      clientSecret: 'mock-oauth-client-secret'
+    }
+  },
 
-	accountMerge:
-		sharelatexHost: 'http://www.sharelatex.dev:3000'
-		betaHost: "http://beta.overleaf.dev:4000"
-		secret: "banana"
+  accountMerge: {
+    sharelatexHost: 'http://www.sharelatex.dev:3000',
+    betaHost: 'http://beta.overleaf.dev:4000',
+    secret: 'banana'
+  },
 
-	siteUrl: 'http://beta.overleaf.dev:4000'
+  siteUrl: 'http://beta.overleaf.dev:4000',
 
-	sso:
-		google:
-			client_id: 'google-client-id'
-			client_secret: 'google-client-secret'
-			callback_path: '/auth/google/callback'
-		orcid:
-			client_id: 'orcid-client-id'
-			client_secret: 'orcid-client-secret'
-			callback_path: '/auth/orcid/callback'
-		twitter:
-			client_id: 'twitter-client-id'
-			client_secret: 'twitter-client-secret'
-			callback_path: '/auth/twitter/callback'
+  sso: {
+    google: {
+      client_id: 'google-client-id',
+      client_secret: 'google-client-secret',
+      callback_path: '/auth/google/callback'
+    },
+    orcid: {
+      client_id: 'orcid-client-id',
+      client_secret: 'orcid-client-secret',
+      callback_path: '/auth/orcid/callback'
+    },
+    twitter: {
+      client_id: 'twitter-client-id',
+      client_secret: 'twitter-client-secret',
+      callback_path: '/auth/twitter/callback'
+    }
+  },
 
-	allowPublicAccess: true
+  allowPublicAccess: true,
 
-	openInOverleaf:
-	  templateUriPrefix: "https://production-overleaf-static.s3.amazonaws.com/v1templates/"
+  openInOverleaf: {
+    templateUriPrefix:
+      'https://production-overleaf-static.s3.amazonaws.com/v1templates/'
+  },
 
-	security:
-		bcryptRounds: 1
+  security: {
+    bcryptRounds: 1
+  }
+}
--- test/acceptance/config/settings.test.js
+++ test/acceptance/config/settings.test.js
@@ -1,206 +1,253 @@
-v1Api =
-	url: "http://#{process.env['V1_HOST'] || 'localhost'}:5000"
-	user: 'overleaf'
-	pass: 'password'
+let features
+const v1Api = {
+  url: `http://${process.env['V1_HOST'] || 'localhost'}:5000`,
+  user: 'overleaf',
+  pass: 'password'
+}
 
-httpAuthUser = "sharelatex"
-httpAuthPass = "password"
-httpAuthUsers = {}
+const httpAuthUser = 'sharelatex'
+const httpAuthPass = 'password'
+const httpAuthUsers = {}
 httpAuthUsers[httpAuthUser] = httpAuthPass
 
-module.exports =
-	mongo:
-		url: process.env['MONGO_CONNECTION_STRING'] || process.env['MONGO_URL'] || "mongodb://#{process.env['MONGO_HOST'] or '127.0.0.1'}/sharelatex_dev"
+module.exports = {
+  mongo: {
+    url:
+      process.env['MONGO_CONNECTION_STRING'] ||
+      process.env['MONGO_URL'] ||
+      `mongodb://${process.env['MONGO_HOST'] || '127.0.0.1'}/sharelatex_dev`
+  },
 
-	cacheStaticAssets: true
-	enableSubscriptions: true
-	allowPublicAccess: true
+  cacheStaticAssets: true,
+  enableSubscriptions: true,
+  allowPublicAccess: true,
 
-	enabledLinkedFileTypes: 'url,project_file,project_output_file,mendeley,zotero'.split(',')
+  enabledLinkedFileTypes: 'url,project_file,project_output_file,mendeley,zotero'.split(
+    ','
+  ),
 
-	httpAuthUsers: httpAuthUsers
-	adminDomains: ['example.com']
+  httpAuthUsers,
+  adminDomains: ['example.com'],
 
-	apis:
-		web:
-			user: httpAuthUser
-			pass: httpAuthPass
-		v1:
-			url: v1Api.url
-			user: v1Api.user
-			pass: v1Api.pass
+  apis: {
+    web: {
+      user: httpAuthUser,
+      pass: httpAuthPass
+    },
+    v1: {
+      url: v1Api.url,
+      user: v1Api.user,
+      pass: v1Api.pass
+    },
 
-		linkedUrlProxy:
-			url: "http://#{process.env['LINKED_URL_PROXY_HOST'] || 'localhost'}:6543"
+    linkedUrlProxy: {
+      url: `http://${process.env['LINKED_URL_PROXY_HOST'] || 'localhost'}:6543`
+    },
 
-		project_history:
-			sendProjectStructureOps: true
-			initializeHistoryForNewProjects: true
-			displayHistoryForNewProjects: true
+    project_history: {
+      sendProjectStructureOps: true,
+      initializeHistoryForNewProjects: true,
+      displayHistoryForNewProjects: true
+    },
 
-		recurly:
-			# Set up our own mock recurly server
-			url: "http://#{process.env['RECURLY_HOST'] || 'localhost'}:6034"
-			subdomain: 'test'
+    recurly: {
+      // Set up our own mock recurly server
+      url: `http://${process.env['RECURLY_HOST'] || 'localhost'}:6034`,
+      subdomain: 'test'
+    }
+  },
 
-	security:
-		bcryptRounds: 1
+  security: {
+    bcryptRounds: 1
+  },
 
-	# for registration via SL, set enableLegacyRegistration to true
-	# for registration via Overleaf v1, set enableLegacyLogin to true
+  // for registration via SL, set enableLegacyRegistration to true
+  // for registration via Overleaf v1, set enableLegacyLogin to true
 
-	# Currently, acceptance tests require enableLegacyRegistration.
-	enableLegacyRegistration: true
+  // Currently, acceptance tests require enableLegacyRegistration.
+  enableLegacyRegistration: true,
 
-	features: features =
-		v1_free:
-			collaborators: 1
-			dropbox: false
-			versioning: false
-			github: true
-			gitBridge: true
-			templates: false
-			references: false
-			referencesSearch: false
-			mendeley: true
-			compileTimeout: 60
-			compileGroup: "standard"
-			trackChanges: false
-		personal:
-			collaborators: 1
-			dropbox: false
-			versioning: false
-			github: false
-			gitBridge: false
-			templates: false
-			references: false
-			referencesSearch: false
-			mendeley: false
-			compileTimeout: 60
-			compileGroup: "standard"
-			trackChanges: false
-		collaborator:
-			collaborators: 10
-			dropbox: true
-			versioning: true
-			github: true
-			gitBridge: true
-			templates: true
-			references: true
-			referencesSearch: true
-			mendeley: true
-			compileTimeout: 180
-			compileGroup: "priority"
-			trackChanges: true
-		professional:
-			collaborators: -1
-			dropbox: true
-			versioning: true
-			github: true
-			gitBridge: true
-			templates: true
-			references: true
-			referencesSearch: true
-			mendeley: true
-			compileTimeout: 180
-			compileGroup: "priority"
-			trackChanges: true
+  features: (features = {
+    v1_free: {
+      collaborators: 1,
+      dropbox: false,
+      versioning: false,
+      github: true,
+      gitBridge: true,
+      templates: false,
+      references: false,
+      referencesSearch: false,
+      mendeley: true,
+      compileTimeout: 60,
+      compileGroup: 'standard',
+      trackChanges: false
+    },
+    personal: {
+      collaborators: 1,
+      dropbox: false,
+      versioning: false,
+      github: false,
+      gitBridge: false,
+      templates: false,
+      references: false,
+      referencesSearch: false,
+      mendeley: false,
+      compileTimeout: 60,
+      compileGroup: 'standard',
+      trackChanges: false
+    },
+    collaborator: {
+      collaborators: 10,
+      dropbox: true,
+      versioning: true,
+      github: true,
+      gitBridge: true,
+      templates: true,
+      references: true,
+      referencesSearch: true,
+      mendeley: true,
+      compileTimeout: 180,
+      compileGroup: 'priority',
+      trackChanges: true
+    },
+    professional: {
+      collaborators: -1,
+      dropbox: true,
+      versioning: true,
+      github: true,
+      gitBridge: true,
+      templates: true,
+      references: true,
+      referencesSearch: true,
+      mendeley: true,
+      compileTimeout: 180,
+      compileGroup: 'priority',
+      trackChanges: true
+    }
+  }),
 
-	defaultFeatures: features.personal
-	defaultPlanCode: 'personal'
-	institutionPlanCode: 'professional'
+  defaultFeatures: features.personal,
+  defaultPlanCode: 'personal',
+  institutionPlanCode: 'professional',
 
-	plans: plans = [{
-		planCode: "v1_free"
-		name: "V1 Free"
-		price: 0
-		features: features.v1_free
-	},{
-		planCode: "personal"
-		name: "Personal"
-		price: 0
-		features: features.personal
-	},{
-		planCode: "collaborator"
-		name: "Collaborator"
-		price: 1500
-		features: features.collaborator
-	},{
-		planCode: "professional"
-		name: "Professional"
-		price: 3000
-		features: features.professional
-	}]
+  plans: [
+    {
+      planCode: 'v1_free',
+      name: 'V1 Free',
+      price: 0,
+      features: features.v1_free
+    },
+    {
+      planCode: 'personal',
+      name: 'Personal',
+      price: 0,
+      features: features.personal
+    },
+    {
+      planCode: 'collaborator',
+      name: 'Collaborator',
+      price: 1500,
+      features: features.collaborator
+    },
+    {
+      planCode: 'professional',
+      name: 'Professional',
+      price: 3000,
+      features: features.professional
+    }
+  ],
 
-	bonus_features:
-		1:
-			collaborators: 2
-			dropbox: false
-			versioning: false
-		3:
-			collaborators: 4
-			dropbox: false
-			versioning: false
-		6:
-			collaborators: 4
-			dropbox: true
-			versioning: true
-		9:
-			collaborators: -1
-			dropbox: true
-			versioning: true
+  bonus_features: {
+    1: {
+      collaborators: 2,
+      dropbox: false,
+      versioning: false
+    },
+    3: {
+      collaborators: 4,
+      dropbox: false,
+      versioning: false
+    },
+    6: {
+      collaborators: 4,
+      dropbox: true,
+      versioning: true
+    },
+    9: {
+      collaborators: -1,
+      dropbox: true,
+      versioning: true
+    }
+  },
 
-	proxyUrls:
-		'/institutions/list': { baseUrl: v1Api.url, path: '/universities/list' }
-		'/institutions/list/:id':
-			baseUrl: v1Api.url
-			path: (params) -> "/universities/list/#{params.id}"
-		'/institutions/domains': { baseUrl: v1Api.url, path: '/university/domains' }
-		'/proxy/missing/baseUrl': path: '/foo/bar'
-		'/proxy/get_and_post': {
-			methods: ['get', 'post'],
-			path: '/destination/get_and_post'
-		}
+  proxyUrls: {
+    '/institutions/list': { baseUrl: v1Api.url, path: '/universities/list' },
+    '/institutions/list/:id': {
+      baseUrl: v1Api.url,
+      path(params) {
+        return `/universities/list/${params.id}`
+      }
+    },
+    '/institutions/domains': {
+      baseUrl: v1Api.url,
+      path: '/university/domains'
+    },
+    '/proxy/missing/baseUrl': {
+      path: '/foo/bar'
+    },
+    '/proxy/get_and_post': {
+      methods: ['get', 'post'],
+      path: '/destination/get_and_post'
+    }
+  },
 
-	redirects:
-		'/redirect/one': '/destination/one',
-		'/redirect/get_and_post': {
-			methods: ['get', 'post'],
-			url: '/destination/get_and_post'
-		},
-		'/redirect/base_url': {
-			baseUrl: 'https://example.com'
-			url: '/destination/base_url'
-		},
-		'/redirect/params/:id': {
-			url: (params) -> "/destination/#{params.id}/params"
-		},
-		'/redirect/qs': '/destination/qs'
-		'/docs_v1': {
-			url: '/docs'
-		}
+  redirects: {
+    '/redirect/one': '/destination/one',
+    '/redirect/get_and_post': {
+      methods: ['get', 'post'],
+      url: '/destination/get_and_post'
+    },
+    '/redirect/base_url': {
+      baseUrl: 'https://example.com',
+      url: '/destination/base_url'
+    },
+    '/redirect/params/:id': {
+      url(params) {
+        return `/destination/${params.id}/params`
+      }
+    },
+    '/redirect/qs': '/destination/qs',
+    '/docs_v1': {
+      url: '/docs'
+    }
+  },
 
-	oauthProviders:
-		'provider': {
-			name: 'provider'
-		},
-		'collabratec': {
-			name: 'collabratec'
-		}
-		'google': {
-			name: 'google'
-		},
+  oauthProviders: {
+    provider: {
+      name: 'provider'
+    },
+    collabratec: {
+      name: 'collabratec'
+    },
+    google: {
+      name: 'google'
+    }
+  },
 
-	# for testing /user/bonus
-	social:
-		twitter:
-			handle: 'overleaf'
+  // for testing /user/bonus
+  social: {
+    twitter: {
+      handle: 'overleaf'
+    },
 
-		facebook:
-			appId:  '400474170024644'
-			picture: 'https://www.overleaf.com/img/ol-brand/logo-horizontal.png'
-			redirectUri: 'https://www.overleaf.com'
+    facebook: {
+      appId: '400474170024644',
+      picture: 'https://www.overleaf.com/img/ol-brand/logo-horizontal.png',
+      redirectUri: 'https://www.overleaf.com'
+    }
+  },
 
-  # setting to true since many features are enabled/disabled after availability of this
-  # property (check Features.js)
-	overleaf: true
+  // setting to true since many features are enabled/disabled after availability of this
+  // property (check Features.js)
+  overleaf: true
+}
