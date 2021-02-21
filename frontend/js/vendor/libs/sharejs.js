4a76c7f0e925889309ddce672726ab3e7b7f812d
Jakob Ackermann <das7pad@outlook.com>
2021-02-21 17:38:00 +0100

    [WIP] esbuild - sharejs as esm

--- frontend/js/vendor/libs/sharejs.js
+++ frontend/js/vendor/libs/sharejs.js
@@ -1,3 +1,6 @@
+import 'ace/ace'
+import CryptoJSSHA1 from 'crypto-js/sha1'
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
@@ -26,7 +29,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
  * DS207: Consider shorter variations of null checks
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-define(['ace/ace','crypto-js/sha1'], function (_ignore, CryptoJSSHA1) {
+(function () {
   var append = void 0,
       bootstrapTransform = void 0,
       exports = void 0,
@@ -1705,4 +1708,6 @@ define(['ace/ace','crypto-js/sha1'], function (_ignore, CryptoJSSHA1) {
   });
 
   return window.sharejs;
-});
+})();
+
+export default window.sharejs
