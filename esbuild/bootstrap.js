d8e797d25f00bdbc8c6de784da84389102d0924e
Jakob Ackermann <das7pad@outlook.com>
2021-02-21 23:46:06 +0100

    [WIP] esbuild - docs

--- esbuild/bootstrap.js
+++ esbuild/bootstrap.js
@@ -1,8 +1,13 @@
+/**
+ * ES6 imports are not strictly sorted. Use require statements instead.
+ */
 const jQuery = require('jquery')
 
+// provide `jQuery` and `$` globally w/o import
 export { jQuery, jQuery as $ }
 
 window.jQuery = jQuery
 require('angular')
 
+// provide `angular` globally w/o import
 export const angular = window.angular
