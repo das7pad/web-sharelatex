b64497ea7a0600584be5ad82e67f856474de6457
Jakob Ackermann <das7pad@outlook.com>
2021-03-05 00:07:05 +0100

    [build/views] fix Angular XSS in meta fields

--- build/views/generator.js
+++ build/views/generator.js
@@ -8,7 +8,17 @@ function generateModule({ path, cache, debug }) {
 }
 
 function generateModuleInMemory({ path, cache, debug }) {
+  const OL_META = new RegExp('(^|\\b)meta\\(id="ol-', 'g')
   return pug.compileFile(path, {
+    plugins: [
+      {
+        // Mitigate Angular XSS globally
+        preLex: src => {
+          src = src.replace(OL_META, 'meta(ng-non-bindable id="ol-')
+          return src
+        }
+      }
+    ],
     cache,
     compileDebug: debug,
     doctype: 'html'
