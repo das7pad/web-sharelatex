fa80c7ff0a0ed8a4d948046fa0a8308b49b89d9b
Jakob Ackermann <das7pad@outlook.com>
2021-03-09 00:03:52 +0100

    [misc] esbuild: we are not going to inline loader images

--- esbuild.config.js
+++ esbuild.config.js
@@ -25,9 +25,6 @@ const COMMON_CFG = {
     '.png': 'file',
     '.svg': 'file',
     '.gif': 'file'
-    // inline loader images
-    // 'overleaf-o.svg': 'dataurl',
-    // 'overleaf-o-grey.svg': 'dataurl',
   }
 }
 
