43a5234c79e91e592388d7b2ff01632c54488565
Jakob Ackermann <das7pad@outlook.com>
2021-04-17 15:13:01 +0200

    [misc] esbuild: lessLoader: move processing into subprocess
    This eliminates the event loop blocking compilation of less from the
     main thread.
    Build time local: before 2s, after: 1.5s.
    Build time CI: before 4.4s, after 3.7s.

--- esbuild/plugins/lessLoader.js
+++ esbuild/plugins/lessLoader.js
@@ -1,13 +1,12 @@
 /**
- vendor of esbuild-plugin-less
+ based on esbuild-plugin-less
  @url https://github.com/iam-medvedev/esbuild-plugin-less/blob/d8f5669467238d71bd9e068ac4f9d5d4fcc71cd4/src/index.ts
  @license WTFPL License
  @copyright 2021 Ilya Medvedev <ilya@medvedev.im>
  */
 
-const path = require('path')
-const { promises: fs } = require('fs')
-const less = require('less')
+const { execFile } = require('child_process')
+const { promisify } = require('util')
 
 /** Less-loader for esbuild */
 module.exports = function lessLoader(options = {}) {
@@ -16,53 +15,7 @@ module.exports = function lessLoader(options = {}) {
     setup: build => {
       // Build .less files
       build.onLoad({ filter: /\.less$/ }, async args => {
-        const entrypointPath = args.path
-        const entrypointSrc = await fs.readFile(entrypointPath, 'utf-8')
-        const dir = path.dirname(entrypointPath)
-
-        const lessOpts = {
-          // CHANGED: filename must be the full path -- see lessc
-          filename: entrypointPath,
-
-          // CHANGED: Importing relative files works!
-          rootpath: './',
-          rewriteUrls: 'all',
-
-          ...options,
-
-          // CHANGED: align with lessc and put the dir first.
-          paths: [dir, ...(options.paths || [])]
-
-          // NOTE: Source map support for css is not there yet in esbuild.
-          // REF: https://github.com/evanw/esbuild/issues/519
-          // sourceMap: { sourceMapFileInline: true }
-        }
-
-        let result
-        try {
-          result = await less.render(entrypointSrc, lessOpts)
-        } catch (error) {
-          const message = convertLessError(error)
-          return {
-            errors: [message],
-            resolveDir: dir,
-            // CHANGED: add support for watch mode
-            watchFiles: [message.location.file]
-          }
-        }
-
-        // CHANGED: add support for watch mode
-        const { css: bundleContents, imports: watchFiles } = result
-
-        // Back fill entrypoint, it is not part of the `imports` list
-        watchFiles.push(entrypointPath)
-
-        return {
-          contents: bundleContents,
-          loader: 'css',
-          resolveDir: dir,
-          watchFiles
-        }
+        return await renderLessInSubprocess(args.path, options)
       })
     }
   }
@@ -85,4 +38,84 @@ function convertLessError(error) {
       lineText
     }
   }
+}
+
+async function main() {
+  const fs = require('fs')
+  const Path = require('path')
+  const less = require('less')
+  const options = JSON.parse(process.argv.pop())
+  const entrypointPath = process.argv.pop()
+  let out
+  const dir = Path.dirname(entrypointPath)
+  const blob = await fs.promises.readFile(entrypointPath, 'utf-8')
+  try {
+    const lessOpts = {
+      // CHANGED: filename must be the full path -- see lessc
+      filename: entrypointPath,
+
+      // CHANGED: Importing relative files works!
+      rootpath: './',
+      rewriteUrls: 'all',
+
+      ...options,
+
+      // CHANGED: align with lessc and put the dir first.
+      paths: [dir, ...(options.paths || [])]
+
+      // NOTE: Source map support for css is not there yet in esbuild.
+      // REF: https://github.com/evanw/esbuild/issues/519
+      // sourceMap: { sourceMapFileInline: true }
+    }
+
+    const result = await less.render(blob, lessOpts)
+    const { css: bundleContents, imports: watchFiles } = result
+
+    // Back fill entrypoint, it is not part of the `imports` list
+    watchFiles.push(entrypointPath)
+
+    out = {
+      contents: bundleContents,
+      loader: 'css',
+      resolveDir: dir,
+      warnings: [],
+      watchFiles
+    }
+  } catch (error) {
+    const message = convertLessError(error)
+    out = {
+      errors: [message],
+      resolveDir: dir,
+      warnings: [],
+      // CHANGED: add support for watch mode
+      watchFiles: [message.location.file]
+    }
+  }
+  console.log(JSON.stringify(out))
+}
+
+async function renderLessInSubprocess(path, options) {
+  const blob = `
+   ${main.toString()}
+   ${convertLessError.toString()}
+   main()
+  `
+  const { stderr: stdErr, stdout: stdOut } = await promisify(
+    execFile
+  )(process.execPath, [
+    '--unhandled-rejections=strict',
+    '-e',
+    blob,
+    path,
+    JSON.stringify(options)
+  ])
+  const result = JSON.parse(stdOut)
+  if (stdErr) {
+    const warnings = stdErr
+      .split('\n')
+      .filter(line => line.trim())
+      .map(text => ({ text }))
+    result.warnings.push(...warnings)
+  }
+  return result
 }
