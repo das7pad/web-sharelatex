80375dba36db3df00295d46922cc6504d92125be
Jakob Ackermann <das7pad@outlook.com>
2021-05-08 15:05:43 +0200

    [misc] eslint: move rules/plugins into smaller scores -- is this faster?

--- esbuild/inject/bootstrap.js
+++ esbuild/inject/bootstrap.js
@@ -1,6 +1,7 @@
 /**
  * Provide $/angular as globals and use full jQuery in angular.
  */
+/* global require */
 const $ = require('jquery')
 
 // Provide `$` globally w/o explicit import.
--- frontend/js/pages/user/beta.js
+++ frontend/js/pages/user/beta.js
@@ -1,3 +1,4 @@
 // Bug in import order: https://github.com/evanw/esbuild/issues/399
+/* global require */
 require('../../features/beta-program/controllers/beta-program-participate')
 require('../../main')
